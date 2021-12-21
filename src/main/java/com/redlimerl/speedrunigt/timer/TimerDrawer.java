5038e80937d78dc5c8e9ef5742ec2a3de80dee6e
RedLime <redlimerl@gmail.com>
2021-12-21 21:00:23 +0900

    Custom font in Timer Added shadow timer text style Added No decimal, 1 decimal

--- src/main/java/com/redlimerl/speedrunigt/timer/TimerDrawer.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/TimerDrawer.java
@@ -2,13 +2,16 @@ package com.redlimerl.speedrunigt.timer;
 
 import com.redlimerl.speedrunigt.option.SpeedRunOptions;
 import com.redlimerl.speedrunigt.option.SpeedRunOptions.TimerDecimals;
+import com.redlimerl.speedrunigt.option.SpeedRunOptions.TimerDecoration;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
+import net.minecraft.util.Identifier;
 
 public class TimerDrawer {
+
+    public static Identifier DEFAULT_FONT = new Identifier("default");
 
     private final boolean translateZ;
     private final MinecraftClient client = MinecraftClient.getInstance();
@@ -17,18 +20,19 @@ public class TimerDrawer {
     private float igtYPos;
     private float igtScale;
     private Integer igtColor;
-    private boolean igtDrawOutline;
+    private TimerDecoration igtDecoration;
 
     private float rtaXPos;
     private float rtaYPos;
     private float rtaScale;
     private Integer rtaColor;
-    private boolean rtaDrawOutline;
+    private TimerDecoration rtaDecoration;
 
     private boolean simply;
     private boolean toggle;
     private boolean isLocked;
     private TimerDecimals timerDecimals;
+    private Identifier timerFont;
 
     public TimerDrawer(boolean translateZ) {
         this(translateZ,
@@ -36,37 +40,39 @@ public class TimerDrawer {
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_IGT_POSITION_Y),
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_IGT_SCALE),
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_IGT_COLOR),
-                SpeedRunOptions.getOption(SpeedRunOptions.TIMER_IGT_OUTLINE),
+                SpeedRunOptions.getOption(SpeedRunOptions.TIMER_IGT_DECO),
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_RTA_POSITION_X),
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_RTA_POSITION_Y),
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_RTA_SCALE),
                 SpeedRunOptions.getOption(SpeedRunOptions.TIMER_RTA_COLOR),
-                SpeedRunOptions.getOption(SpeedRunOptions.TIMER_RTA_OUTLINE),
+                SpeedRunOptions.getOption(SpeedRunOptions.TIMER_RTA_DECO),
                 SpeedRunOptions.getOption(SpeedRunOptions.DISPLAY_TIME_ONLY),
                 SpeedRunOptions.getOption(SpeedRunOptions.TOGGLE_TIMER),
                 SpeedRunOptions.getOption(SpeedRunOptions.LOCK_TIMER_POSITION),
-                SpeedRunOptions.getOption(SpeedRunOptions.DISPLAY_DECIMALS));
+                SpeedRunOptions.getOption(SpeedRunOptions.DISPLAY_DECIMALS),
+                SpeedRunOptions.getOption(SpeedRunOptions.TIMER_TEXT_FONT));
     }
 
     public TimerDrawer(boolean translateZ,
-                       float igtXPos, float igtYPos, float igtScale, Integer igtColor, boolean igtDrawOutline,
-                       float rtaXPos, float rtaYPos, float rtaScale, Integer rtaColor, boolean rtaDrawOutline,
-                       boolean simply, boolean toggle, boolean isLocked, TimerDecimals timerDecimals) {
+                       float igtXPos, float igtYPos, float igtScale, Integer igtColor, TimerDecoration igtDecoration,
+                       float rtaXPos, float rtaYPos, float rtaScale, Integer rtaColor, TimerDecoration rtaDecoration,
+                       boolean simply, boolean toggle, boolean isLocked, TimerDecimals timerDecimals, Identifier timerFont) {
         this.translateZ = translateZ;
         this.igtXPos = igtXPos;
         this.igtYPos = igtYPos;
         this.igtScale = igtScale;
         this.igtColor = igtColor;
-        this.igtDrawOutline = igtDrawOutline;
+        this.igtDecoration = igtDecoration;
         this.rtaXPos = rtaXPos;
         this.rtaYPos = rtaYPos;
         this.rtaScale = rtaScale;
         this.rtaColor = rtaColor;
-        this.rtaDrawOutline = rtaDrawOutline;
+        this.rtaDecoration = rtaDecoration;
         this.simply = simply;
         this.toggle = toggle;
         this.isLocked = isLocked;
         this.timerDecimals = timerDecimals;
+        this.timerFont = timerFont;
     }
 
     public float getIGT_XPos() {
@@ -121,16 +127,8 @@ public class TimerDrawer {
         return igtColor;
     }
 
-    public boolean isIGTDrawOutline() {
-        return igtDrawOutline;
-    }
-
     public Integer getRTAColor() {
         return rtaColor;
-    }
-
-    public boolean isRTADrawOutline() {
-        return rtaDrawOutline;
     }
 
     public void setIGTScale(float igtScale) {
@@ -153,18 +151,25 @@ public class TimerDrawer {
         this.igtColor = igtColor;
     }
 
-    public void setIGTDrawOutline(boolean igtDrawOutline) {
-        this.igtDrawOutline = igtDrawOutline;
-    }
-
     public void setRTAColor(Integer rtaColor) {
         this.rtaColor = rtaColor;
     }
 
-    public void setRTADrawOutline(boolean rtaDrawOutline) {
-        this.rtaDrawOutline = rtaDrawOutline;
+    public TimerDecoration getIGTDecoration() {
+        return igtDecoration;
     }
 
+    public void setIGTDecoration(TimerDecoration igtDecoration) {
+        this.igtDecoration = igtDecoration;
+    }
+
+    public TimerDecoration getRTADecoration() {
+        return rtaDecoration;
+    }
+
+    public void setRTADecoration(TimerDecoration rtaDecoration) {
+        this.rtaDecoration = rtaDecoration;
+    }
 
     public boolean isLocked() {
         return isLocked;
@@ -182,6 +187,15 @@ public class TimerDrawer {
         this.timerDecimals = timerDecimals;
     }
 
+    public Identifier getTimerFont() {
+        return timerFont;
+    }
+
+    public void setTimerFont(Identifier timerFont) {
+        this.timerFont = timerFont;
+    }
+
+
     private String getTimeFormat(long time) {
         if (!InGameTimer.getInstance().isPlaying() && translateZ) {
             return InGameTimer.timeToStringFormat(time);
@@ -191,8 +205,14 @@ public class TimerDrawer {
         if (minutes > 59) {
             int hours = minutes / 60;
             minutes = minutes % 60;
+            if (timerDecimals == TimerDecimals.NONE) {
+                return String.format("%d:%02d:%02d", hours, minutes, seconds);
+            }
             return String.format("%d:%02d:%02d.%0" + timerDecimals.getNumber() + ".0f", hours, minutes, seconds, time % Math.pow(10, timerDecimals.getNumber()));
         } else {
+            if (timerDecimals == TimerDecimals.NONE) {
+                return String.format("%02d:%02d", minutes, seconds);
+            }
             return String.format("%02d:%02d.%0" + timerDecimals.getNumber() + ".0f", minutes, seconds, time % Math.pow(10, timerDecimals.getNumber()));
         }
     }
@@ -213,12 +233,18 @@ public class TimerDrawer {
         MutableText igtText = getIGTText();
         MutableText rtaText = getRTAText();
 
+        //폰트 조정
+        if (getTimerFont() != DEFAULT_FONT && client.fontManager.fontStorages.containsKey(getTimerFont())) {
+            rtaText.setStyle(rtaText.getStyle().withFont(getTimerFont()));
+            igtText.setStyle(igtText.getStyle().withFont(getTimerFont()));
+        }
+
         TimerElement igtTimerElement = new TimerElement();
         TimerElement rtaTimerElement = new TimerElement();
 
         //초기 값 조정
-        rtaTimerElement.init(rtaXPos, rtaYPos, rtaScale, rtaText, rtaColor, rtaDrawOutline);
-        igtTimerElement.init(igtXPos, igtYPos, igtScale, igtText, igtColor, igtDrawOutline);
+        rtaTimerElement.init(rtaXPos, rtaYPos, rtaScale, rtaText, rtaColor, rtaDecoration);
+        igtTimerElement.init(igtXPos, igtYPos, igtScale, igtText, igtColor, igtDecoration);
 
         //렌더
         MatrixStack matrixStack = new MatrixStack();
@@ -226,20 +252,6 @@ public class TimerDrawer {
         if (rtaScale != 0) rtaTimerElement.draw(matrixStack, translateZ);
 
         client.getProfiler().pop();
-    }
-
-    static void drawOutLine(TextRenderer textRenderer, MatrixStack matrixStack, int x, int y, MutableText text, Integer color, boolean drawOutline) {
-        if (drawOutline) {
-            textRenderer.draw(matrixStack, text, (float)x + 1, (float)y + 1, 0);
-            textRenderer.draw(matrixStack, text, (float)x + 1, (float)y, 0);
-            textRenderer.draw(matrixStack, text, (float)x + 1, (float)y - 1, 0);
-            textRenderer.draw(matrixStack, text, (float)x, (float)y - 1, 0);
-            textRenderer.draw(matrixStack, text, (float)x, (float)y + 1, 0);
-            textRenderer.draw(matrixStack, text, (float)x - 1, (float)y + 1, 0);
-            textRenderer.draw(matrixStack, text, (float)x - 1, (float)y, 0);
-            textRenderer.draw(matrixStack, text, (float)x - 1, (float)y - 1, 0);
-        }
-        textRenderer.draw(matrixStack, text, (float)x, (float)y, color);
     }
 
 
