48224a4e3085c376daa17d935065c886858bf783
RedLime <redlimerl@gmail.com>
2021-12-24 20:58:15 +0900

    Added easy font add system Fixed AA Advancement counting bug Fixed some segments run problem

--- src/main/java/com/redlimerl/speedrunigt/option/SpeedRunIGTInfoScreen.java
+++ src/main/java/com/redlimerl/speedrunigt/option/SpeedRunIGTInfoScreen.java
@@ -44,7 +44,7 @@ public class SpeedRunIGTInfoScreen extends Screen {
         assert client != null;
         update = addDrawableChild(new ButtonWidget(width / 2 - 155, height - 104, 150, 20, new TranslatableText("speedrunigt.menu.download_update"), (ButtonWidget button) -> Util.getOperatingSystem().open(UPDATE_URL)));
         update.active = false;
-        addDrawableChild(new ButtonWidget(width / 2 + 5, height - 104, 150, 20, new TranslatableText("speedrunigt.menu.latest_change_log"), (ButtonWidget button) -> Util.getOperatingSystem().open("https://github.com/RedLime/SpeedRunIGT/releases")));
+        addDrawableChild(new ButtonWidget(width / 2 + 5, height - 104, 150, 20, new TranslatableText("speedrunigt.menu.latest_change_log"), (ButtonWidget button) -> Util.getOperatingSystem().open("https://github.com/RedLime/SpeedRunIGT/releases/latest")));
 
         addDrawableChild(new ButtonWidget(width / 2 - 155, height - 80, 150, 20, new TranslatableText("speedrunigt.menu.open_github_repo"), (ButtonWidget button) -> Util.getOperatingSystem().open("https://github.com/RedLime/SpeedRunIGT/")));
         addDrawableChild(new ButtonWidget(width / 2 + 5, height - 80, 150, 20, new TranslatableText("speedrunigt.menu.open_support_page"), (ButtonWidget button) -> Util.getOperatingSystem().open("https://www.patreon.com/join/redlimerl/checkout")));
