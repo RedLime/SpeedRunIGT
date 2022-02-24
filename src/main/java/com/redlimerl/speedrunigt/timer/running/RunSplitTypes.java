4f965ff0ab7b8d09127bda42ab058d165efa0239
RedLime <redlimerl@gmail.com>
2022-02-24 19:21:59 +0900

    split translate aaAAA

--- src/main/java/com/redlimerl/speedrunigt/timer/running/RunSplitTypes.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/running/RunSplitTypes.java
@@ -8,7 +8,7 @@ public class RunSplitTypes {
     public static RunSplitType ENTER_END = new RunSplitType("ENTER_END", "advancements.story.enter_the_end.title");
     public static RunSplitType ENTER_STRONG_HOLD = new RunSplitType("ENTER_STRONG_HOLD", "advancements.story.follow_ender_eye.title");
     public static RunSplitType ENTER_FORTRESS = new RunSplitType("ENTER_FORTRESS", "advancements.nether.find_fortress.title");
-    public static RunSplitType ENTER_BASTION = new RunSplitType("ENTER_BASTION", "advancements.nether.find_bastion.title");
+    public static RunSplitType ENTER_BASTION = new RunSplitType("ENTER_BASTION", "Found the bastion");
     public static RunSplitType COMPLETE = new RunSplitType("COMPLETE", "speedrunigt.split.complete_run");
 
 }
