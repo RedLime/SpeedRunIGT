8f881ef6c68f82ad296c0fb7bbefdfc1ae41aa24
RedLime <redlimerl@gmail.com>
2022-02-24 18:44:06 +0900

    rename RecordListScreen

--- src/main/java/com/redlimerl/speedrunigt/gui/screen/TimerRecordListScreen.java
+++ src/main/java/com/redlimerl/speedrunigt/gui/screen/TimerRecordListScreen.java
@@ -22,7 +22,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class TimerSplitListScreen extends Screen {
+public class TimerRecordListScreen extends Screen {
 
     private final Screen parent;
     private TimerSplitListWidget listWidget;
@@ -33,7 +33,7 @@ public class TimerSplitListScreen extends Screen {
     }
     private RunOrder runOrder = RunOrder.FASTEST;
 
-    public TimerSplitListScreen(Screen parent) {
+    public TimerRecordListScreen(Screen parent) {
         super(new TranslatableText("speedrunigt.split.title"));
         this.parent = parent;
     }
@@ -92,10 +92,10 @@ public class TimerSplitListScreen extends Screen {
         private List<String> tooltip = null;
 
         public TimerSplitListWidget() {
-            super(TimerSplitListScreen.this.client,
-                    TimerSplitListScreen.this.width, TimerSplitListScreen.this.height,
-                    32, TimerSplitListScreen.this.height - 42, 24);
-            this.applyFilter(TimerSplitListScreen.this.filter, TimerSplitListScreen.this.runOrder);
+            super(TimerRecordListScreen.this.client,
+                    TimerRecordListScreen.this.width, TimerRecordListScreen.this.height,
+                    32, TimerRecordListScreen.this.height - 42, 24);
+            this.applyFilter(TimerRecordListScreen.this.filter, TimerRecordListScreen.this.runOrder);
         }
 
         void applyFilter(RunType filter, RunOrder runOrder) {
@@ -189,14 +189,14 @@ public class TimerSplitListScreen extends Screen {
                 this.deleteButton.y = y + 2;
                 this.deleteButton.render(mouseX, mouseY, tickDelta);
 
-                drawString(TimerSplitListScreen.this.textRenderer, titleText.asFormattedString(), x, y + 4, 16777215);
+                drawString(TimerRecordListScreen.this.textRenderer, titleText.asFormattedString(), x, y + 4, 16777215);
                 if (timelineTextList.size() > 0) {
                     int count = (int) ((System.currentTimeMillis() - entryCreatedTime) / 2500) % timelineTextList.size();
-                    drawString(TimerSplitListScreen.this.textRenderer, resultTimeText.deepCopy().append("  ").append(timelineTextList.get(count)).asFormattedString(), x, y + 13, 16777215);
+                    drawString(TimerRecordListScreen.this.textRenderer, resultTimeText.deepCopy().append("  ").append(timelineTextList.get(count)).asFormattedString(), x, y + 13, 16777215);
                 } else {
-                    drawString(TimerSplitListScreen.this.textRenderer, resultTimeText.asFormattedString(), x, y + 13, 16777215);
+                    drawString(TimerRecordListScreen.this.textRenderer, resultTimeText.asFormattedString(), x, y + 13, 16777215);
                 }
-                drawCenteredString(TimerSplitListScreen.this.textRenderer, "§8§m                                                                                ",
+                drawCenteredString(TimerRecordListScreen.this.textRenderer, "§8§m                                                                                ",
                         x + (entryWidth / 2), y + 20, 16777215);
 
                 ArrayList<String> tooltip = new ArrayList<>();
