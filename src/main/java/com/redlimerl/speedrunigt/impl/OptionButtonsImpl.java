276bd0e56d1980db45abe1ffe3edf74522b694f3
RedLime <redlimerl@gmail.com>
2022-02-24 19:26:44 +0900

    optimize imports

--- src/main/java/com/redlimerl/speedrunigt/impl/OptionButtonsImpl.java
+++ src/main/java/com/redlimerl/speedrunigt/impl/OptionButtonsImpl.java
@@ -8,8 +8,8 @@ import com.redlimerl.speedrunigt.gui.screen.TimerCustomizeScreen;
 import com.redlimerl.speedrunigt.gui.screen.TimerRecordListScreen;
 import com.redlimerl.speedrunigt.option.SpeedRunOption;
 import com.redlimerl.speedrunigt.option.SpeedRunOptions;
-import net.fabricmc.loader.api.ModContainer;
 import com.redlimerl.speedrunigt.version.ScreenTexts;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
@@ -18,7 +18,6 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 
 import static com.redlimerl.speedrunigt.SpeedRunIGT.TIMER_DRAWER;
