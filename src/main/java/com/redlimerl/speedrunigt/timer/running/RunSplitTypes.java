1e3876f7a3b256ac79539c32898910b1f51a0dbc
RedLime <redlimerl@gmail.com>
2022-02-24 19:22:55 +0900

    split translate aaAAA

--- src/main/java/com/redlimerl/speedrunigt/timer/running/RunSplitTypes.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/running/RunSplitTypes.java
@@ -4,11 +4,11 @@ public class RunSplitTypes {
 
     public static RunSplitType ERROR_SPLIT = new RunSplitType("error", "Not found split :(");
 
-    public static RunSplitType ENTER_NETHER = new RunSplitType("ENTER_NETHER", "advancements.story.enter_the_nether.title");
-    public static RunSplitType ENTER_END = new RunSplitType("ENTER_END", "advancements.story.enter_the_end.title");
-    public static RunSplitType ENTER_STRONG_HOLD = new RunSplitType("ENTER_STRONG_HOLD", "advancements.story.follow_ender_eye.title");
-    public static RunSplitType ENTER_FORTRESS = new RunSplitType("ENTER_FORTRESS", "advancements.nether.find_fortress.title");
-    public static RunSplitType ENTER_BASTION = new RunSplitType("ENTER_BASTION", "advancements.nether.find_bastion.title");
+    public static RunSplitType ENTER_NETHER = new RunSplitType("ENTER_NETHER", "Enter the Nether");
+    public static RunSplitType ENTER_END = new RunSplitType("ENTER_END", "Enter the End");
+    public static RunSplitType ENTER_STRONG_HOLD = new RunSplitType("ENTER_STRONG_HOLD", "Found the Stronghold");
+    public static RunSplitType ENTER_FORTRESS = new RunSplitType("ENTER_FORTRESS", "Found the Fortress");
+    public static RunSplitType ENTER_BASTION = new RunSplitType("ENTER_BASTION", "Found the bastion");
     public static RunSplitType COMPLETE = new RunSplitType("COMPLETE", "speedrunigt.split.complete_run");
 
 }
