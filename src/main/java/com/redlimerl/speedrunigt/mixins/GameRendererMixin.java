1f3805b95fa3078206f62369ce1fec12bb5920ff
RedLime <redlimerl@gmail.com>
2022-02-28 13:59:02 +0900

    fix render for dynamic fps

--- src/main/java/com/redlimerl/speedrunigt/mixins/GameRendererMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/GameRendererMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 
-    @Inject(method = "method_1331", at = @At(value = "TAIL"))
+    @Inject(method = "method_9775", at = @At(value = "TAIL"))
     private void drawTimer(CallbackInfo ci) {
         MixinValues.IS_RENDERED_BEFORE = true;
     }
