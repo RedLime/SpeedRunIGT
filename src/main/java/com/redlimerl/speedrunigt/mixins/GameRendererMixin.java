7f28c9ba2265197e8125a9c1a18634e5d0f0f87c
RedLime <redlimerl@gmail.com>
2022-02-28 13:58:15 +0900

    fix render for dynamic fps

---
+++ src/main/java/com/redlimerl/speedrunigt/mixins/GameRendererMixin.java
@@ -0,0 +1,17 @@
+package com.redlimerl.speedrunigt.mixins;
+
+import com.redlimerl.speedrunigt.utils.MixinValues;
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    @Inject(method = "method_1331", at = @At(value = "TAIL"))
+    private void drawTimer(CallbackInfo ci) {
+        MixinValues.IS_RENDERED_BEFORE = true;
+    }
+}
