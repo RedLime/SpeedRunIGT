1f67ee54c491891914c573d0f3420d2b49d091cf
RedLime <redlimerl@gmail.com>
2022-03-02 15:48:58 +0900

    Added InGameTimerUtils

--- src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
@@ -4,10 +4,10 @@ import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.gui.screen.TimerCustomizeScreen;
 import com.redlimerl.speedrunigt.mixins.access.FontManagerAccessor;
 import com.redlimerl.speedrunigt.mixins.access.MinecraftClientAccessor;
-import com.redlimerl.speedrunigt.mixins.access.WorldRendererAccessor;
 import com.redlimerl.speedrunigt.option.SpeedRunOption;
 import com.redlimerl.speedrunigt.option.SpeedRunOptions;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.TimerDrawer;
 import com.redlimerl.speedrunigt.timer.TimerStatus;
 import com.redlimerl.speedrunigt.timer.running.RunCategories;
@@ -20,7 +20,6 @@ import net.minecraft.client.gui.screen.CreditsScreen;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourceManager;
@@ -38,7 +37,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -56,20 +54,18 @@ public abstract class MinecraftClientMixin {
 
     @Shadow @Nullable public Screen currentScreen;
 
-    @Shadow @Final public WorldRenderer worldRenderer;
-
     @Shadow @Nullable public ClientWorld world;
 
-    @Shadow public abstract boolean isWindowFocused();
+    @Shadow @Final private ReloadableResourceManagerImpl resourceManager;
 
     @Shadow private Profiler profiler;
 
-    @Shadow @Final private ReloadableResourceManagerImpl resourceManager;
+    @Shadow private boolean paused;
 
     @Inject(at = @At("HEAD"), method = "createWorld")
     public void onCreate(String worldName, LevelInfo levelInfo, DynamicRegistryManager dynamicRegistryManager, GeneratorOptions generatorOptions, CallbackInfo ci) {
         InGameTimer.start(worldName);
-        currentDimension = null;
+        InGameTimerUtils.IS_CHANGING_DIMENSION = true;
     }
 
     @Inject(at = @At("HEAD"), method = "startIntegratedServer(Ljava/lang/String;)V")
@@ -77,24 +73,20 @@ public abstract class MinecraftClientMixin {
         try {
             boolean loaded = InGameTimer.load(worldName);
             if (!loaded) InGameTimer.end();
-            currentDimension = null;
         } catch (Exception e) {
             InGameTimer.end();
-            currentDimension = null;
             SpeedRunIGT.error("Exception in timer load, can't load the timer.");
             e.printStackTrace();
         }
+        InGameTimerUtils.IS_CHANGING_DIMENSION = true;
     }
-
-    private static DimensionType currentDimension = null;
 
     @Inject(at = @At("HEAD"), method = "joinWorld")
     public void onJoin(ClientWorld targetWorld, CallbackInfo ci) {
         InGameTimer timer = InGameTimer.getInstance();
         if (timer.getStatus() == TimerStatus.NONE) return;
 
-        currentDimension = targetWorld.getDimension();
-        InGameTimer.checkingWorld = true;
+        InGameTimerUtils.IS_CHANGING_DIMENSION = false;
 
         if (timer.getStatus() != TimerStatus.NONE) {
             timer.setPause(true, TimerStatus.IDLE);
@@ -112,17 +104,15 @@ public abstract class MinecraftClientMixin {
         }
     }
 
-    @ModifyVariable(method = "render(Z)V", at = @At(value = "STORE"), ordinal = 1)
-    private boolean renderMixin(boolean paused) {
+    @Inject(method = "render(Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;getMeasuringTimeNano()J", shift = At.Shift.AFTER))
+    private void renderMixin(boolean tick, CallbackInfo ci) {
         InGameTimer timer = InGameTimer.getInstance();
 
-        if (timer.getStatus() == TimerStatus.RUNNING && paused) {
+        if (timer.getStatus() == TimerStatus.RUNNING && this.paused) {
             timer.setPause(true, TimerStatus.PAUSED);
-        } else if (timer.getStatus() == TimerStatus.PAUSED && !paused) {
+        } else if (timer.getStatus() == TimerStatus.PAUSED && !this.paused) {
             timer.setPause(false);
         }
-
-        return paused;
     }
 
 
@@ -132,18 +122,11 @@ public abstract class MinecraftClientMixin {
         this.profiler.swap("timer");
         InGameTimer timer = InGameTimer.getInstance();
 
-        if (worldRenderer != null && world != null && world.getDimension() == currentDimension && !isPaused() && isWindowFocused()
-                && timer.getStatus() == TimerStatus.IDLE && InGameTimer.checkingWorld) {
-            WorldRendererAccessor worldRendererAccessor = (WorldRendererAccessor) worldRenderer;
-            int chunks = worldRendererAccessor.invokeCompletedChunkCount();
-            int entities = worldRendererAccessor.getRegularEntityCount() - (options.getPerspective().isFirstPerson() ? 0 : 1);
-
-            if (chunks + entities > 0) {
-                if (!(SpeedRunOption.getOption(SpeedRunOptions.WAITING_FIRST_INPUT) && !timer.isStarted())) {
-                    timer.setPause(false);
-                } else {
-                    timer.updateFirstRendered();
-                }
+        if (InGameTimerUtils.canUnpauseTimer(true)) {
+            if (!(SpeedRunOption.getOption(SpeedRunOptions.WAITING_FIRST_INPUT) && !timer.isStarted())) {
+                timer.setPause(false);
+            } else {
+                timer.updateFirstRendered();
             }
         }
 
