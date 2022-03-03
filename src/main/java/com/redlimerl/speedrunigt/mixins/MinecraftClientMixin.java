6551b1c3f326c1214794e4ca2a25a670e7c098e7
RedLime <redlimerl@gmail.com>
2022-03-03 14:11:41 +0900

    change log stuff

--- src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
@@ -87,7 +87,7 @@ public abstract class MinecraftClientMixin {
         InGameTimerUtils.IS_CHANGING_DIMENSION = false;
 
         if (timer.getStatus() != TimerStatus.NONE) {
-            timer.setPause(true, TimerStatus.IDLE);
+            timer.setPause(true, TimerStatus.IDLE, "changed dimension");
         }
 
         //Enter Nether
@@ -107,9 +107,9 @@ public abstract class MinecraftClientMixin {
         InGameTimer timer = InGameTimer.getInstance();
 
         if (timer.getStatus() == TimerStatus.RUNNING && this.paused) {
-            timer.setPause(true, TimerStatus.PAUSED);
+            timer.setPause(true, TimerStatus.PAUSED, "player");
         } else if (timer.getStatus() == TimerStatus.PAUSED && !this.paused) {
-            timer.setPause(false);
+            timer.setPause(false, "player");
         }
     }
 
@@ -122,7 +122,7 @@ public abstract class MinecraftClientMixin {
 
         if (InGameTimerUtils.canUnpauseTimer(true)) {
             if (!(SpeedRunOption.getOption(SpeedRunOptions.WAITING_FIRST_INPUT) && !timer.isStarted())) {
-                timer.setPause(false);
+                timer.setPause(false, "rendered");
             } else {
                 timer.updateFirstRendered();
             }
