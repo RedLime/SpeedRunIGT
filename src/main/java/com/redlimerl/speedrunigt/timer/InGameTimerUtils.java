456d154cf6720c776bc5af46e9cd0b716d852555
RedLime <redlimerl@gmail.com>
2022-03-07 16:35:16 +0900

    remove seed stuff

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 
 public class InGameTimerUtils {
     public static boolean IS_CHANGING_DIMENSION = false;
-    public static boolean LATEST_SEED_IS_RANDOM = false;
 
     public static Path getWorldSavePath(String name) {
         return MinecraftClient.getInstance().getCurrentSave().method_11957(name, "./").toPath();
@@ -69,8 +68,6 @@ public class InGameTimerUtils {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("mc_version", getMinecraftVersion());
         jsonObject.addProperty("speedrunigt_version", SpeedRunIGT.MOD_VERSION);
-        jsonObject.addProperty("seed", timer.getSeedName());
-        jsonObject.addProperty("is_set_seed", timer.isSetSeed());
         jsonObject.addProperty("category", timer.getCategory().getID());
         jsonObject.addProperty("is_coop", timer.isCoop());
         jsonObject.addProperty("is_hardcore", timer.isHardcore());
