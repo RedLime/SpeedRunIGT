f1648270549dd3517fb7556c27fbc8111453e6cc
RedLime <redlimerl@gmail.com>
2022-03-08 14:55:14 +0900

    fix button

--- src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionButtonWidgetMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionButtonWidgetMixin.java
@@ -1,21 +1,22 @@
 package com.redlimerl.speedrunigt.mixins.retime;
 
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.OptionButtonWidget;
 import net.minecraft.client.gui.widget.OptionSliderWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ButtonWidget.class)
+@Mixin(Screen.class)
 public class OptionButtonWidgetMixin {
 
-    @SuppressWarnings("ConstantConditions")
-    @Inject(method = "mouseClicked", at = @At("RETURN"))
-    public void onClickOption(double d, double e, int i, CallbackInfoReturnable<Boolean> cir) {
-        if (((Object) this) instanceof OptionButtonWidget || ((Object) this) instanceof OptionSliderWidget) {
+    @Inject(method = "buttonClicked", at = @At("RETURN"))
+    public void onClickOption(ButtonWidget button, CallbackInfo ci) {
+        if (button instanceof OptionButtonWidget || button instanceof OptionSliderWidget) {
             InGameTimerUtils.RETIME_IS_CHANGED_OPTION = true;
         }
     }
