61eb8c7dd7bb02f684e5a3f3dd2d43b38408348a
RedLime <redlimerl@gmail.com>
2022-03-08 13:47:04 +0900

    Add auto-retime

--- src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionButtonWidgetMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionButtonWidgetMixin.java
@@ -1,18 +1,18 @@
 package com.redlimerl.speedrunigt.mixins.retime;
 
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
-import net.minecraft.client.gui.widget.AbstractPressableButtonWidget;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractPressableButtonWidget.class)
+@Mixin(ButtonWidget.class)
 public class OptionButtonWidgetMixin {
 
     @SuppressWarnings("ConstantConditions")
-    @Inject(method = "onClick", at = @At("TAIL"))
-    public void onClickOption(double mouseX, double mouseY, CallbackInfo ci) {
+    @Inject(method = "onPress", at = @At("TAIL"))
+    public void onClickOption(CallbackInfo ci) {
         if (((Object) this) instanceof net.minecraft.client.gui.widget.OptionButtonWidget) {
             InGameTimerUtils.RETIME_IS_CHANGED_OPTION = true;
         }
