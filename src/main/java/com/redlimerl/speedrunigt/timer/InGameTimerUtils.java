06547c8d5a972c3854046684c2963ef556b25115
RedLime <redlimerl@gmail.com>
2022-03-08 14:43:00 +0900

    Add auto-retime

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
@@ -3,9 +3,11 @@ package com.redlimerl.speedrunigt.timer;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.redlimerl.speedrunigt.SpeedRunIGT;
+import com.redlimerl.speedrunigt.mixins.access.ClientChunkProviderAccessor;
 import com.redlimerl.speedrunigt.mixins.access.LevelStorageAccessor;
 import com.redlimerl.speedrunigt.mixins.access.ServerStatHandlerAccessor;
 import com.redlimerl.speedrunigt.mixins.access.WorldRendererAccessor;
+import com.redlimerl.speedrunigt.timer.logs.TimerPauseLog;
 import com.redlimerl.speedrunigt.timer.logs.TimerTimeline;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.realms.RealmsSharedConstants;
@@ -17,9 +19,14 @@ import org.lwjgl.opengl.Display;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InGameTimerUtils {
     public static boolean IS_CHANGING_DIMENSION = false;
+    public static boolean RETIME_IS_CHANGED_OPTION = false;
+    public static boolean RETIME_IS_WAITING_LOAD = false;
 
     public static Path getWorldSavePath(String name) {
         return ((LevelStorageAccessor) MinecraftClient.getInstance().getCurrentSave()).getFile().toPath().resolve(name);
@@ -45,11 +52,52 @@ public class InGameTimerUtils {
         return false;
     }
 
+    public static float getGeneratedChunkRatio() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.world != null && client.player != null) {
+            int chunks = (client.options.viewDistance * 2 + 1)^2;
+            return (float) ((ClientChunkProviderAccessor) client.world.getChunkProvider()).getChunkMap().getUsedEntriesCount() / chunks;
+        }
+        return 0;
+    }
+
     public static String logListToString(ArrayList<?> arrayList) {
         if (arrayList.size() == 0) return "";
         StringBuilder stringBuilder = new StringBuilder();
         for (Object o : arrayList) {
             stringBuilder.append(o.toString()).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String makeLogText(int length, Object text) {
+        String empty = IntStream.range(0, Math.max(0, length - text.toString().length())).mapToObj(i -> " ").collect(Collectors.joining());
+        return text + empty;
+    }
+
+    public static String pauseLogListToString(List<TimerPauseLog> arrayList) {
+        if (arrayList.size() == 0) return "";
+
+        StringBuilder stringBuilder = new StringBuilder()
+                .append(makeLogText(5, "No"))
+                .append(makeLogText(15, "IGT"))
+                .append(makeLogText(15, "Start RTA"))
+                .append(makeLogText(15, "End RTA"))
+                .append(makeLogText(11, "Length"))
+                .append(makeLogText(11, "Retime"))
+                .append("Reason")
+                .append("\n");
+
+        for (TimerPauseLog pause : arrayList) {
+            stringBuilder
+                    .append(makeLogText(5, pause.getPauseCount()))
+                    .append(makeLogText(15, timeToStringFormat(pause.getIGT())))
+                    .append(makeLogText(15, timeToStringFormat(pause.getUnpauseRTA() - pause.getPauseLength())))
+                    .append(makeLogText(15, timeToStringFormat(pause.getUnpauseRTA())))
+                    .append(makeLogText(11, millisecondToStringFormat(pause.getPauseLength())))
+                    .append(makeLogText(11, pause.getRetimeNeedAmount() == 0 ? "" : millisecondToStringFormat(pause.getRetimeNeedAmount())))
+                    .append(String.format("Paused by %s, Unpause by %s", pause.getPauseReason(), pause.getUnpauseReason()))
+                    .append("\n");
         }
         return stringBuilder.toString();
     }
@@ -66,6 +114,11 @@ public class InGameTimerUtils {
         }
     }
 
+    public static String millisecondToStringFormat(long time) {
+        int seconds = (int) (time / 1000);
+        return String.format("%d.%d", seconds, time % 1000);
+    }
+
     public static JsonObject convertTimelineJson(InGameTimer timer) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("mc_version", getMinecraftVersion());
@@ -75,6 +128,7 @@ public class InGameTimerUtils {
         jsonObject.addProperty("is_hardcore", timer.isHardcore());
         jsonObject.addProperty("is_legacy_igt", timer.isLegacyIGT());
         jsonObject.addProperty("date", System.currentTimeMillis());
+        jsonObject.addProperty("retimed_igt", timer.getRetimedInGameTime());
         jsonObject.addProperty("final_igt", timer.getInGameTime(false));
         jsonObject.addProperty("final_rta", timer.getRealTimeAttack());
         JsonArray timelineArr = new JsonArray();
