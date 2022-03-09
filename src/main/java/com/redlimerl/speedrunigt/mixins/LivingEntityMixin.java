ef6e1d8af5a77191e6689b913121bb942cd4d0e7
RedLime <redlimerl@gmail.com>
2022-03-09 15:09:40 +0900

    Fixed all bosses count Added advancement log, record

--- src/main/java/com/redlimerl/speedrunigt/mixins/LivingEntityMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/LivingEntityMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
