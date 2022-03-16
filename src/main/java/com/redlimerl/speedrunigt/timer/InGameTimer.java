98fd1b5927d39d9ca163b18f5e22a0379280d003
RedLime <redlimerl@gmail.com>
2022-03-16 11:48:53 +0900

    fix crash

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimer.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimer.java
@@ -13,7 +13,6 @@ import com.redlimerl.speedrunigt.timer.running.RunCategories;
 import com.redlimerl.speedrunigt.timer.running.RunCategory;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.apache.commons.io.FileUtils;
