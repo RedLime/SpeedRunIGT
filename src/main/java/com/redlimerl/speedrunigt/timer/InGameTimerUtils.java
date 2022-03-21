385606631c08f364943a7c19a63c94ec13409285
RedLime <redlimerl@gmail.com>
2022-03-21 17:52:00 +0900

    Fixed timer keep works when leave world Added record file in world folder Added run_type record parameter

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
@@ -36,6 +36,7 @@ public class InGameTimerUtils {
     public static boolean IS_CAN_WAIT_WORLD_LOAD = false;
     public static boolean RETIME_IS_CHANGED_OPTION = false;
     public static boolean RETIME_IS_WAITING_LOAD = false;
+    public static boolean IS_SET_SEED = false;
 
     public static File getTimerLogDir(String name, String pathName) {
         File file = ((LevelStorageAccessor) MinecraftClient.getInstance().getCurrentSave()).getFile().toPath().resolve(name).resolve(SpeedRunIGT.MOD_ID).resolve(pathName).toFile();
@@ -160,6 +161,7 @@ public class InGameTimerUtils {
         jsonObject.addProperty("mc_version", getMinecraftVersion());
         jsonObject.addProperty("speedrunigt_version", SpeedRunIGT.MOD_VERSION);
         jsonObject.addProperty("category", timer.getCategory().getID());
+        jsonObject.addProperty("run_type", timer.getRunType().getContext());
         jsonObject.addProperty("is_completed", timer.isCompleted());
         jsonObject.addProperty("is_coop", timer.isCoop());
         jsonObject.addProperty("is_hardcore", timer.isHardcore());
