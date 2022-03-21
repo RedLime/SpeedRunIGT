345fbf80b6c31c678b4eb8dceb1a347245b90d45
RedLime <redlimerl@gmail.com>
2022-03-21 17:52:00 +0900

    Fixed timer keep works when leave world Added record file in world folder Added run_type record parameter

--- src/main/java/com/redlimerl/speedrunigt/timer/running/RunType.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/running/RunType.java
@@ -1,19 +1,31 @@
 package com.redlimerl.speedrunigt.timer.running;
 
 public enum RunType {
-    RANDOM_SEED("Random Seed"), SET_SEED("Set Seed"), FILTERED_SEED("Filtered Seed"), SAVED_WORLD("Saved World");
+    RANDOM_SEED("random_seed", 0), SET_SEED("set_seed", 1), OLD_WORLD("old_world", 2);
 
     private final String context;
+    private final int code;
 
-    RunType(String s) {
+    RunType(String s, int code) {
         this.context = s;
+        this.code = code;
     }
 
     public String getContext() {
         return context;
     }
 
-    public static RunType getRunType(boolean isSetSeed, boolean isFiltered) {
-        return !isSetSeed ? RANDOM_SEED : isFiltered ? FILTERED_SEED : SET_SEED;
+    public int getCode() {
+        return code;
+    }
+
+    public static RunType fromBoolean(boolean isSetSeed) {
+        return isSetSeed ? SET_SEED : RANDOM_SEED;
+    }
+
+    public static RunType fromInt(int code) {
+        if (code == 0) return RANDOM_SEED;
+        if (code == 1) return SET_SEED;
+        return OLD_WORLD;
     }
 }
