62c2ed6b5e931763582a7e5a1dddeb0a66206742
RedLime <redlimerl@gmail.com>
2022-03-22 15:35:42 +0900

    Added timer start option Fixed some log params Fixed ssg/rsg detection

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
@@ -7,6 +7,8 @@ import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.mixins.access.ClientChunkProviderAccessor;
 import com.redlimerl.speedrunigt.mixins.access.ServerStatHandlerAccessor;
 import com.redlimerl.speedrunigt.mixins.access.WorldRendererAccessor;
+import com.redlimerl.speedrunigt.option.SpeedRunOption;
+import com.redlimerl.speedrunigt.option.SpeedRunOptions;
 import com.redlimerl.speedrunigt.timer.logs.TimerPauseLog;
 import com.redlimerl.speedrunigt.timer.logs.TimerTimeline;
 import com.redlimerl.speedrunigt.utils.MixinValues;
@@ -61,6 +63,10 @@ public class InGameTimerUtils {
             return true;
         }
         return false;
+    }
+
+    public static boolean isWaitingFirstInput() {
+        return SpeedRunOption.getOption(SpeedRunOptions.WAITING_FIRST_INPUT).isFirstInput(InGameTimer.getInstance());
     }
 
     public static float getGeneratedChunkRatio() {
