c1601976d35bfbb87383f35c900b541a0d32d450
RedLime <redlimerl@gmail.com>
2022-03-24 16:33:18 +0900

    optimize imports

--- src/main/java/com/redlimerl/speedrunigt/mixins/ClientPlayerEntityMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ClientPlayerEntityMixin.java
@@ -22,7 +22,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
