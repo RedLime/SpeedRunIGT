77d57c9fecc33d2c9964e6f3ab022780ee5cd9fb
RedLime <redlimerl@gmail.com>
2022-03-28 18:01:58 +0900

    Fixed AA time exclude bug Added open_lan record parameter

--- src/main/java/com/redlimerl/speedrunigt/mixins/server/IntegratedServerMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/server/IntegratedServerMixin.java
@@ -3,7 +3,7 @@ package com.redlimerl.speedrunigt.mixins.server;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.TimerStatus;
 import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.world.level.LevelInfo;
+import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class IntegratedServerMixin {
 
     @Inject(method = "method_3000", at = @At("RETURN"))
-    public void onOpenLan(LevelInfo.GameMode gamemode, boolean bl, CallbackInfoReturnable<String> cir) {
+    public void onOpenLan(GameMode bl, boolean par2, CallbackInfoReturnable<String> cir) {
         if (InGameTimer.getInstance().getStatus() != TimerStatus.NONE) InGameTimer.getInstance().openedLanIntegratedServer();
     }
 
