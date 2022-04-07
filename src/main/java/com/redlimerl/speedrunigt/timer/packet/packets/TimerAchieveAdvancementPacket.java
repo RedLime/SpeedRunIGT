ad4670fc59347b15298dcec06381151624eb4a2e
RedLime <redlimerl@gmail.com>
2022-04-07 15:01:30 +0900

    remove aa sync

--- src/main/java/com/redlimerl/speedrunigt/timer/packet/packets/TimerAchieveAdvancementPacket.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/packet/packets/TimerAchieveAdvancementPacket.java
@@ -1,13 +1,10 @@
 package com.redlimerl.speedrunigt.timer.packet.packets;
 
-import com.google.common.collect.Maps;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.packet.TimerPacket;
 import com.redlimerl.speedrunigt.timer.packet.TimerPacketBuf;
-import net.minecraft.advancement.AdvancementDisplay;
 import net.minecraft.advancement.SimpleAdvancement;
-import net.minecraft.class_3258;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
@@ -30,7 +27,6 @@ public class TimerAchieveAdvancementPacket extends TimerPacket {
     protected TimerPacketBuf convertClient2ServerPacket(TimerPacketBuf buf, MinecraftClient client) {
         if (sendAdvancement != null) {
             buf.writeIdentifier(sendAdvancement.getIdentifier());
-            sendAdvancement.asTaskAdvancement().writeToByteBuf(buf.getBuffer());
         }
         return buf;
     }
@@ -44,7 +40,6 @@ public class TimerAchieveAdvancementPacket extends TimerPacket {
     protected TimerPacketBuf convertServer2ClientPacket(TimerPacketBuf buf, MinecraftServer server) {
         if (sendAdvancement != null) {
             buf.writeIdentifier(sendAdvancement.getIdentifier());
-            sendAdvancement.asTaskAdvancement().writeToByteBuf(buf.getBuffer());
         }
         return buf;
     }
@@ -52,17 +47,7 @@ public class TimerAchieveAdvancementPacket extends TimerPacket {
     @Override
     public void receiveServer2ClientPacket(TimerPacketBuf buf, MinecraftClient client) {
         Identifier identifier = buf.readIdentifier();
-        if (buf.readBoolean()) buf.readIdentifier(); // Get Parent ID
-        AdvancementDisplay advancementDisplay = buf.readBoolean() ? AdvancementDisplay.fromPacketByteBuf(buf.getBuffer()) : null;
-        if (advancementDisplay != null && identifier != null) {
-            SimpleAdvancement advancement = new SimpleAdvancement(identifier, null, advancementDisplay, null, Maps.newHashMap(), null);
-
-            if (advancementDisplay.method_15014() && !InGameTimerUtils.COMPLETED_ADVANCEMENTS.contains(identifier.toString())) {
-                MinecraftClient.getInstance().method_14462().method_14491(new class_3258(advancement));
-            }
-
-            InGameTimer.getInstance().tryInsertNewAdvancement(identifier.toString(), null, true);
-            InGameTimerUtils.COMPLETED_ADVANCEMENTS.add(identifier.toString());
-        }
+        InGameTimer.getInstance().tryInsertNewAdvancement(identifier.toString(), null, true);
+        InGameTimerUtils.COMPLETED_ADVANCEMENTS.add(identifier.toString());
     }
 }
