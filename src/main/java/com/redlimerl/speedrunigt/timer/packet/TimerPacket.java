c386e2c2bf7709b9b194ea8a99c7ac662cb73de5
RedLime <redlimerl@gmail.com>
2022-04-11 13:22:15 +0900

    Server side updated removed profiler fixed coop advancement tracker works fixed packet memory leak timer custom category error screen is not showing when it is not support version

--- src/main/java/com/redlimerl/speedrunigt/timer/packet/TimerPacket.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/packet/TimerPacket.java
@@ -1,6 +1,9 @@
 package com.redlimerl.speedrunigt.timer.packet;
 
 import com.google.common.collect.Maps;
+import com.redlimerl.speedrunigt.SpeedRunIGT;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
@@ -43,6 +46,7 @@ public abstract class TimerPacket {
         return identifier;
     }
 
+    @Environment(EnvType.CLIENT)
     final CustomPayloadC2SPacket createClient2ServerPacket(MinecraftClient client) {
         TimerPacketBuf buf = TimerPacketBuf.create();
         return new CustomPayloadC2SPacket(identifier, convertClient2ServerPacket(buf, client).getBuffer());
@@ -61,11 +65,13 @@ public abstract class TimerPacket {
         TimerPacketUtils.sendServer2ClientPacket(server, this, buf);
     }
 
+    @Environment(EnvType.CLIENT)
     protected abstract TimerPacketBuf convertClient2ServerPacket(TimerPacketBuf buf, MinecraftClient client);
 
     public abstract void receiveClient2ServerPacket(TimerPacketBuf buf, MinecraftServer server);
 
     protected abstract TimerPacketBuf convertServer2ClientPacket(TimerPacketBuf buf, MinecraftServer server);
 
+    @Environment(EnvType.CLIENT)
     public abstract void receiveServer2ClientPacket(TimerPacketBuf buf, MinecraftClient client);
 }
