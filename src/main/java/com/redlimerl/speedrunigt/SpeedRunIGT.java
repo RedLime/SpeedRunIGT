732f9ac5c342c0ed4f822cd8809d4ec3c9e08167
RedLime <redlimerl@gmail.com>
2022-04-11 13:22:15 +0900

    Server side updated removed profiler fixed coop advancement tracker works fixed packet memory leak timer custom category error screen is not showing when it is not support version

--- src/main/java/com/redlimerl/speedrunigt/SpeedRunIGT.java
+++ src/main/java/com/redlimerl/speedrunigt/SpeedRunIGT.java
@@ -3,49 +3,41 @@ package com.redlimerl.speedrunigt;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.redlimerl.speedrunigt.api.CategoryConditionRegisterHelper;
-import com.redlimerl.speedrunigt.api.OptionButtonFactory;
 import com.redlimerl.speedrunigt.api.SpeedRunIGTApi;
-import com.redlimerl.speedrunigt.gui.screen.SpeedRunIGTInfoScreen;
 import com.redlimerl.speedrunigt.impl.CategoryRegistryImpl;
 import com.redlimerl.speedrunigt.impl.ConditionsRegistryImpl;
-import com.redlimerl.speedrunigt.impl.OptionButtonsImpl;
-import com.redlimerl.speedrunigt.option.SpeedRunOption;
-import com.redlimerl.speedrunigt.timer.TimerDrawer;
 import com.redlimerl.speedrunigt.timer.category.CustomCategoryManager;
 import com.redlimerl.speedrunigt.timer.category.RunCategory;
 import com.redlimerl.speedrunigt.timer.category.condition.CategoryCondition;
 import com.redlimerl.speedrunigt.timer.packet.TimerPackets;
 import com.redlimerl.speedrunigt.utils.FontIdentifier;
-import com.redlimerl.speedrunigt.utils.FontUtils;
-import com.redlimerl.speedrunigt.utils.KeyBindingRegistry;
-import com.redlimerl.speedrunigt.utils.TranslateHelper;
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
-import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.lwjgl.glfw.GLFW;
+import org.spongepowered.include.com.google.common.collect.Sets;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
-public class SpeedRunIGT implements ClientModInitializer {
+public class SpeedRunIGT implements ModInitializer {
 
     public static final String MOD_ID = "speedrunigt";
-    public static TimerDrawer TIMER_DRAWER = new TimerDrawer(true);
     private static boolean isInitialized = false;
     public static boolean isInitialized() { return isInitialized; }
+    public static boolean IS_CLIENT_SIDE = false;
+    public static MinecraftServer DEDICATED_SERVER = null;
 
     public static String DEBUG_DATA = "";
     public static String MOD_VERSION;
@@ -61,7 +53,7 @@ public class SpeedRunIGT implements ClientModInitializer {
     public static Path getGlobalPath() { return new File(System.getProperty("user.home").replace("\\", "/"), SpeedRunIGT.MOD_ID).toPath(); }
     public static Path getRecordsPath() { return getGlobalPath().resolve("records"); }
 
-    public static final ArrayList<ModContainer> API_PROVIDERS = new ArrayList<>();
+    public static final Set<ModContainer> API_PROVIDERS = Sets.newHashSet();
 
     static {
         getMainPath().toFile().mkdirs();
@@ -80,16 +72,11 @@ public class SpeedRunIGT implements ClientModInitializer {
         }
     }
 
-    public static KeyBinding timerResetKeyBinding;
-    public static KeyBinding timerStopKeyBinding;
-
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         MOD_VERSION = (FabricLoader.getInstance().getModContainer(SpeedRunIGT.MOD_ID).isPresent()
                         ? FabricLoader.getInstance().getModContainer(SpeedRunIGT.MOD_ID).get().getMetadata().getVersion().getFriendlyString() : "Unknown+Unknown");
 
-        // init default option buttons
-        SpeedRunOption.addOptionButtonFactories(new OptionButtonsImpl().createOptionButtons().toArray(new OptionButtonFactory[0]));
         // init default categories
         new CategoryRegistryImpl().registerCategories().forEach(RunCategory::registerCategory);
         CategoryCondition.registerCondition(new ConditionsRegistryImpl().registerConditions());
@@ -98,14 +85,6 @@ public class SpeedRunIGT implements ClientModInitializer {
         // Registry API's
         for (EntrypointContainer<SpeedRunIGTApi> entryPoint : FabricLoader.getInstance().getEntrypointContainers("speedrunigt", SpeedRunIGTApi.class)) {
             SpeedRunIGTApi api = entryPoint.getEntrypoint();
-
-            // Registry single option button
-            OptionButtonFactory singleFactory = api.createOptionButton();
-            if (singleFactory != null) SpeedRunOption.addOptionButtonFactories(singleFactory);
-
-            // Registry multiple option buttons
-            Collection<OptionButtonFactory> multipleFactory = api.createOptionButtons();
-            if (multipleFactory != null) SpeedRunOption.addOptionButtonFactories(multipleFactory.toArray(new OptionButtonFactory[0]));
 
             // Registry single category
             RunCategory singleCategory = api.registerCategory();
@@ -125,30 +104,6 @@ public class SpeedRunIGT implements ClientModInitializer {
         // Custom Json Category initialize
         CustomCategoryManager.init();
 
-        // Options initialize
-        SpeedRunOption.init();
-
-        // Translate initialize
-        try {
-            TranslateHelper.init();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
-        // Key Bindings initialize
-        timerResetKeyBinding = KeyBindingRegistry.registerKeyBinding(new KeyBinding(
-                "speedrunigt.controls.start_timer",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_U,
-                "speedrunigt.title.options"
-        ));
-        timerStopKeyBinding = KeyBindingRegistry.registerKeyBinding(new KeyBinding(
-                "speedrunigt.controls.stop_timer",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_I,
-                "speedrunigt.title.options"
-        ));
-
         // End initializing
         isInitialized = true;
 
@@ -157,10 +112,7 @@ public class SpeedRunIGT implements ClientModInitializer {
         System.setProperty("speedrunigt.record", "");
 
         // Update checking
-        SpeedRunIGTInfoScreen.checkUpdate();
-
-        // Add default fonts
-        FontUtils.copyDefaultFonts();
+        SpeedRunIGTUpdateChecker.checkUpdate();
 
         // Initializing packets
         TimerPackets.init();
