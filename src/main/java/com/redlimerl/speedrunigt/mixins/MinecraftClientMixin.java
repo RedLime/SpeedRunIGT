b7fed78b3dfce697d1b453ded7121d8528317415
RedLime <redlimerl@gmail.com>
2022-04-11 14:37:34 +0900

    option init server side also optimize imports

--- src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
@@ -5,7 +5,10 @@ import com.redlimerl.speedrunigt.SpeedRunIGTClient;
 import com.redlimerl.speedrunigt.gui.screen.TimerCustomizeScreen;
 import com.redlimerl.speedrunigt.option.SpeedRunOption;
 import com.redlimerl.speedrunigt.option.SpeedRunOptions;
-import com.redlimerl.speedrunigt.timer.*;
+import com.redlimerl.speedrunigt.timer.InGameTimer;
+import com.redlimerl.speedrunigt.timer.InGameTimerClientUtils;
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import com.redlimerl.speedrunigt.timer.TimerStatus;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
 import com.redlimerl.speedrunigt.timer.running.RunType;
 import com.redlimerl.speedrunigt.utils.MixinValues;
@@ -21,7 +24,6 @@ import net.minecraft.world.level.LevelInfo;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -158,7 +160,7 @@ public abstract class MinecraftClientMixin {
     private float previousX=0;
     private float previousY=0;
 
-    @Redirect(method="tick", at=@At(value="INVOKE", target = "Lorg/lwjgl/input/Mouse;getEventDWheel()I"))
+    @Redirect(method="tick", at=@At(value="INVOKE", target = "Lorg/lwjgl/input/Mouse;getEventDWheel()I", remap = false))
     public int getScrolled(){
         if(Mouse.getEventDWheel()!=0){
             unlock();
