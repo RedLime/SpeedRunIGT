78ca9d53d8d2fc8ffbc78843f7bdcdd44e2598ef
RedLime <redlimerl@gmail.com>
2022-04-11 18:11:41 +0900

    fix packet problem

--- src/main/java/com/redlimerl/speedrunigt/mixins/coop/PlayerManagerMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/coop/PlayerManagerMixin.java
@@ -5,6 +5,7 @@ import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.packet.TimerPacketUtils;
 import com.redlimerl.speedrunigt.timer.packet.packets.TimerInitPacket;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Final;
@@ -14,21 +15,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
 
     @Shadow public abstract int getCurrentPlayerCount();
 
-    @Shadow @Final private List<ServerPlayerEntity> players;
+    @Shadow @Final private MinecraftServer server;
 
     @Inject(method = "onPlayerConnect", at = @At("TAIL"))
     public void onPlayerConnectInject(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
         if (!InGameTimer.getInstance().isCompleted() && ((InGameTimer.getInstance().isStarted() && this.getCurrentPlayerCount() > 1) || !SpeedRunIGT.IS_CLIENT_SIDE)) {
-            long startTime = System.currentTimeMillis();
-            if (InGameTimer.getInstance().isStarted()) startTime = InGameTimer.getInstance().getStartTime();
-            TimerPacketUtils.sendServer2ClientPacket(this.players, new TimerInitPacket(InGameTimer.getInstance(), startTime));
+            long rta = InGameTimer.getInstance().isStarted() ? InGameTimer.getInstance().getRealTimeAttack() : 0;
+            TimerPacketUtils.sendServer2ClientPacket(this.server, new TimerInitPacket(InGameTimer.getInstance(), rta));
         }
     }
 }
