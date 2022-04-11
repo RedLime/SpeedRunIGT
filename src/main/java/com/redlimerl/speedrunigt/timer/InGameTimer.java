7a695f1e7134e4e9f137353e732504a768c5345c
RedLime <redlimerl@gmail.com>
2022-04-11 13:04:55 +0900

    Server side updated removed profiler fixed coop advancement tracker works fixed packet memory leak timer custom category error screen is not showing when it is not support version

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimer.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimer.java
@@ -128,7 +128,7 @@ public class InGameTimer implements Serializable {
             INSTANCE.writeRecordFile();
         }
         INSTANCE = new InGameTimer(worldName);
-        INSTANCE.setCategory(SpeedRunOption.getOption(SpeedRunOptions.TIMER_CATEGORY));
+        INSTANCE.setCategory(SpeedRunOption.getOption(SpeedRunOptions.TIMER_CATEGORY), false);
         INSTANCE.setPause(true, TimerStatus.IDLE, "startup");
         INSTANCE.isGlitched = SpeedRunOption.getOption(SpeedRunOptions.TIMER_LEGACY_IGT_MODE);
         INSTANCE.runType = runType;
@@ -144,13 +144,13 @@ public class InGameTimer implements Serializable {
         boolean isCoop = INSTANCE.isCoop;
 
         INSTANCE = new InGameTimer(INSTANCE.worldName, false);
-        INSTANCE.setCategory(RunCategories.CUSTOM);
+        INSTANCE.setCategory(RunCategories.CUSTOM, false);
         INSTANCE.runType = runType;
         INSTANCE.isGlitched = isGlitched;
         INSTANCE.isCoop = isCoop;
         INSTANCE.setPause(true, TimerStatus.IDLE, "reset");
         INSTANCE.setPause(false, "reset");
-        if (isCoop) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerInitPacket(INSTANCE, INSTANCE.getStartTime()));
+        if (isCoop && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerInitPacket(INSTANCE, INSTANCE.getStartTime()));
     }
 
     /**
@@ -184,7 +184,7 @@ public class InGameTimer implements Serializable {
         timer.endIGTTime = timer.endTime - timer.leastTickTime;
         timer.setStatus(TimerStatus.COMPLETED_LEGACY);
 
-        if (timer.isCoop() && canSendPacket) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerCompletePacket(endTime));
+        if (timer.isCoop() && canSendPacket && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerCompletePacket(endTime));
 
         if (INSTANCE.isServerIntegrated) {
             StringBuilder resultLog = new StringBuilder();
@@ -365,7 +365,7 @@ public class InGameTimer implements Serializable {
         return RunCategory.getCategory(category);
     }
 
-    public void setCategory(RunCategory category) {
+    public void setCategory(RunCategory category, boolean canSendPacket) {
         this.category = category.getID();
         if (category.getConditionJson() != null) {
             try {
@@ -374,7 +374,7 @@ public class InGameTimer implements Serializable {
                 InGameTimerUtils.setCategoryWarningScreen(category.getConditionFileName(), exception);
             }
         }
-        TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerChangeCategoryPacket(this.category));
+        if (isCoop() && canSendPacket && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerChangeCategoryPacket(this.category));
     }
 
     public boolean isCoop() {
@@ -395,7 +395,7 @@ public class InGameTimer implements Serializable {
 
     public void updateMoreData(int key, int value, boolean canSendPacket) {
         moreData.put(key, value);
-        if (this.isCoop() && canSendPacket) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerDataConditionPacket(key, value));
+        if (this.isCoop() && canSendPacket && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerDataConditionPacket(key, value));
     }
 
     public @NotNull TimerStatus getStatus() {
@@ -411,7 +411,7 @@ public class InGameTimer implements Serializable {
         if (!this.isCompleted) return;
         this.completeCount++;
         this.isCompleted = false;
-        if (canSendPacket && this.isCoop()) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerUncompletedPacket());
+        if (canSendPacket && this.isCoop() && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerUncompletedPacket());
     }
 
     public boolean isCompleted() {
@@ -452,8 +452,8 @@ public class InGameTimer implements Serializable {
         if (this.isCompleted && this != COMPLETED_INSTANCE) return COMPLETED_INSTANCE.getInGameTime(smooth);
         if (this.isCoop) return getRealTimeAttack();
 
-        if (this.isGlitched && this.isServerIntegrated && MinecraftClient.getInstance().getServer() != null) {
-            Long inGameTime = InGameTimerUtils.getPlayerTime();
+        if (this.isGlitched && this.isServerIntegrated && InGameTimerUtils.getServer() != null && SpeedRunIGT.IS_CLIENT_SIDE) {
+            Long inGameTime = InGameTimerClientUtils.getPlayerTime();
             if (inGameTime != null) return inGameTime;
         }
 
@@ -616,7 +616,7 @@ public class InGameTimer implements Serializable {
             if (Objects.equals(timeline.getName(), name)) return false;
         }
         timelines.add(new TimerTimeline(name, getInGameTime(false), getRealTimeAttack()));
-        if (canSendPacket && this.isCoop()) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerTimelinePacket(name));
+        if (canSendPacket && this.isCoop() && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerTimelinePacket(name));
         return true;
     }
 
@@ -676,7 +676,7 @@ public class InGameTimer implements Serializable {
         if (completed) {
             condition.setCompleted(true);
             tryInsertNewTimeline(condition.getName());
-            if (this.isCoop()) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerCustomConditionPacket(condition));
+            if (this.isCoop() && SpeedRunIGT.IS_CLIENT_SIDE) TimerPacketUtils.sendClient2ServerPacket(MinecraftClient.getInstance(), new TimerCustomConditionPacket(condition));
         }
     }
 
