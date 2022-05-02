73dbab6e5e50e99a83bac6e352bac74a73ef300c
RedLime <redlimerl@gmail.com>
2022-05-02 15:26:16 +0900

    fix all portals & first input

--- src/main/java/com/redlimerl/speedrunigt/mixins/ClientWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ClientWorldMixin.java
@@ -2,7 +2,6 @@ package com.redlimerl.speedrunigt.mixins;
 
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
-import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -13,7 +12,6 @@ import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.SaveHandler;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.Dimension;
-import net.minecraft.world.dimension.OverworldDimension;
 import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -36,19 +34,10 @@ public abstract class ClientWorldMixin extends World {
     }
 
     @Override
-    public void method_11481(BlockPos pos, BlockState oldState, BlockState newState, int flags) {
-        super.method_11481(pos, oldState, newState, flags);
+    public boolean setBlockState(BlockPos pos, BlockState state, int flags) {
+        boolean result = super.setBlockState(pos, state, flags);
 
         InGameTimer timer = InGameTimer.getInstance();
-        if (flags == 2 && newState.getBlock() == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && dimension instanceof OverworldDimension) {
-            for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
-                if (runPortalPos.squaredDistanceTo(pos) < 100) {
-                    return;
-                }
-            }
-            timer.getEndPortalPosList().add(new RunPortalPos(pos));
-
-        }
         if (timer.getCategory() == RunCategories.MINE_A_CHUNK) {
             ChunkPos chunkPos = getChunk(pos).getChunkPos();
             for (int x = chunkPos.getActualX(); x < chunkPos.getOppositeX() + 1; x++) {
@@ -56,13 +45,14 @@ public abstract class ClientWorldMixin extends World {
                     for (int z = chunkPos.getActualZ(); z < chunkPos.getOppositeZ() + 1; z++) {
                         Block block = getBlockState(new BlockPos(x, y, z)).getBlock();
                         if (block != Blocks.BEDROCK && block != Blocks.AIR) {
-                            return;
+                            return result;
                         }
                     }
                 }
             }
             InGameTimer.complete();
         }
+        return result;
     }
 
     private int getBedrockMaxHeight() {
