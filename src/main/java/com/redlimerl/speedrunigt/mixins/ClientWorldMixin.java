1f301f94410760f24db5e321d3c9f6c28036649e
RedLime <redlimerl@gmail.com>
2022-05-02 16:21:59 +0900

    release 10.2

--- src/main/java/com/redlimerl/speedrunigt/mixins/ClientWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ClientWorldMixin.java
@@ -1,18 +1,61 @@
 package com.redlimerl.speedrunigt.mixins;
 
 import com.redlimerl.speedrunigt.timer.InGameTimer;
+import com.redlimerl.speedrunigt.timer.category.RunCategories;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.SaveHandler;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * It is `ClientWorldMixin` class but ~1.13 have block updater is in World class. so... don't confuse it lol
+ */
 @Mixin(ClientWorld.class)
-public class ClientWorldMixin {
+public abstract class ClientWorldMixin extends World {
+
+    protected ClientWorldMixin(SaveHandler handler, LevelProperties properties, Dimension dim, Profiler profiler, boolean isClient) {
+        super(handler, properties, dim, profiler, isClient);
+    }
 
     @Inject(at = @At("HEAD"), method = "tick")
     public void onTick(CallbackInfo ci) {
         InGameTimer.getInstance().tick();
     }
 
+    @Override
+    public boolean setBlockState(BlockPos pos, BlockState state, int flags) {
+        boolean result = super.setBlockState(pos, state, flags);
+
+        InGameTimer timer = InGameTimer.getInstance();
+        if (timer.getCategory() == RunCategories.MINE_A_CHUNK) {
+            ChunkPos chunkPos = getChunk(pos).getChunkPos();
+            for (int x = chunkPos.getActualX(); x < chunkPos.getOppositeX() + 1; x++) {
+                for (int y = getBedrockMaxHeight(); y < getMaxBuildHeight(); y++) {
+                    for (int z = chunkPos.getActualZ(); z < chunkPos.getOppositeZ() + 1; z++) {
+                        Block block = getBlockState(new BlockPos(x, y, z)).getBlock();
+                        if (block != Blocks.BEDROCK && block != Blocks.AIR) {
+                            return result;
+                        }
+                    }
+                }
+            }
+            InGameTimer.complete();
+        }
+        return result;
+    }
+
+    private int getBedrockMaxHeight() {
+        return 5;
+    }
 }
