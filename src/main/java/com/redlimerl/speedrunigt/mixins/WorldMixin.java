a9710e91163a8665ea365ab84ac80c78197aeb4e
RedLime <redlimerl@gmail.com>
2022-05-02 15:19:36 +0900

    fix all portal & first input

---
+++ src/main/java/com/redlimerl/speedrunigt/mixins/WorldMixin.java
@@ -0,0 +1,41 @@
+package com.redlimerl.speedrunigt.mixins;
+
+import com.redlimerl.speedrunigt.timer.InGameTimer;
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import com.redlimerl.speedrunigt.timer.category.RunCategories;
+import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
+
+@Mixin(World.class)
+public abstract class WorldMixin {
+
+    @Shadow public abstract boolean isClient();
+
+    @Shadow public abstract RegistryKey<World> getRegistryKey();
+
+    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", at = @At("TAIL"))
+    public void onUpdateBlockState(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir) {
+        InGameTimer timer = InGameTimer.getInstance();
+        if (!this.isClient() && flags == 2 && state.getBlock() == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && Objects.equals(getRegistryKey().getValue().toString(), DimensionType.OVERWORLD_ID.toString())) {
+            for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
+                if (runPortalPos.squaredDistanceTo(pos) < 100) {
+                    return;
+                }
+            }
+            timer.getEndPortalPosList().add(new RunPortalPos(pos));
+            InGameTimerUtils.IS_KILLED_ENDER_DRAGON = false;
+        }
+    }
+}
