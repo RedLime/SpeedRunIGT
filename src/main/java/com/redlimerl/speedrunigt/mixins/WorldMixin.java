3170bdfcc43893be0c5a977aa89f976dc06c3070
RedLime <redlimerl@gmail.com>
2022-05-02 18:22:31 +0900

    fix all portals

--- src/main/java/com/redlimerl/speedrunigt/mixins/WorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/WorldMixin.java
@@ -6,35 +6,40 @@ import com.redlimerl.speedrunigt.timer.category.RunCategories;
 import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.SaveHandler;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.OverworldDimension;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(World.class)
-public abstract class WorldMixin {
+@Mixin(ServerWorld.class)
+public abstract class WorldMixin extends World {
 
-    @Shadow @Final public Dimension dimension;
+    protected WorldMixin(SaveHandler handler, LevelProperties properties, Dimension dim, Profiler profiler, boolean isClient) {
+        super(handler, properties, dim, profiler, isClient);
+    }
 
-    @Shadow @Final public boolean isClient;
-
-    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", at = @At("TAIL"))
-    public void onUpdateBlockState(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir) {
+    @Override
+    public boolean setBlockState(BlockPos pos, BlockState state, int flags) {
+        boolean result = super.setBlockState(pos, state, flags);
         InGameTimer timer = InGameTimer.getInstance();
-        if (!this.isClient && flags == 2 && state.getBlock() == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && timer.getCategory() == RunCategories.ALL_PORTALS && dimension instanceof OverworldDimension) {
+        if (!this.isClient() && flags == 2 && state.getBlock() == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && dimension instanceof OverworldDimension) {
             for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
                 if (runPortalPos.squaredDistanceTo(pos) < 100) {
-                    return;
+                    return result;
                 }
             }
             timer.getEndPortalPosList().add(new RunPortalPos(pos));
             InGameTimerUtils.IS_KILLED_ENDER_DRAGON = false;
         }
+        return result;
+    }
+
+    public boolean isClient() {
+        return this.isClient;
     }
 }
