eb5f16083df8716f2091b49d261ae559a8684122
RedLime <redlimerl@gmail.com>
2022-05-03 19:34:48 +0900

    fix all portals timing

--- src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
@@ -1,39 +1,46 @@
 package com.redlimerl.speedrunigt.mixins;
 
+import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
 import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(World.class)
-public abstract class WorldMixin {
+import java.util.function.Supplier;
 
-    @Shadow public abstract RegistryKey<DimensionType> getDimensionRegistryKey();
+@Mixin(ServerWorld.class)
+public abstract class ServerWorldMixin extends World {
 
-    @Shadow public abstract boolean isClient();
+    protected ServerWorldMixin(MutableWorldProperties mutableWorldProperties, RegistryKey<World> registryKey, RegistryKey<DimensionType> registryKey2, DimensionType dimensionType, Supplier<Profiler> profiler, boolean bl, boolean bl2, long l) {
+        super(mutableWorldProperties, registryKey, registryKey2, dimensionType, profiler, bl, bl2, l);
+    }
 
-    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", at = @At("TAIL"))
-    public void onUpdateBlockState(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir) {
+    @Override
+    public boolean setBlockState(BlockPos pos, BlockState state, int flags) {
+        boolean result = super.setBlockState(pos, state, flags);
+
         InGameTimer timer = InGameTimer.getInstance();
         if (!this.isClient() && flags == 2 && state.getBlock() == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && getDimensionRegistryKey() == DimensionType.OVERWORLD_REGISTRY_KEY) {
             for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
                 if (runPortalPos.squaredDistanceTo(pos) < 100) {
-                    return;
+                    return result;
                 }
             }
+            SpeedRunIGT.debug("Detected serverworld end portal");
             timer.getEndPortalPosList().add(new RunPortalPos(pos));
             InGameTimerUtils.IS_KILLED_ENDER_DRAGON = false;
         }
+
+        return result;
     }
 }
