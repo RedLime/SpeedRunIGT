27ffebfed2fdddf00fe33f241f4a732eb763377b
RedLime <redlimerl@gmail.com>
2022-05-03 19:58:04 +0900

    fix all portals timing

--- src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
@@ -1,5 +1,6 @@
 package com.redlimerl.speedrunigt.mixins;
 
+import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
@@ -17,29 +18,28 @@ import net.minecraft.world.level.LevelInfo;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ServerWorld.class)
-public abstract class WorldMixin extends World {
+public abstract class ServerWorldMixin extends World {
 
-    public WorldMixin(SaveHandler saveHandler, String string, Dimension dimension, LevelInfo levelInfo, Profiler profiler) {
+    public ServerWorldMixin(SaveHandler saveHandler, String string, Dimension dimension, LevelInfo levelInfo, Profiler profiler) {
         super(saveHandler, string, dimension, levelInfo, profiler);
     }
 
     @Override
     public boolean method_4721(int i, int j, int k, Block block, int l, int m) {
         boolean result = super.method_4721(i, j, k, block, l, m);
+
         InGameTimer timer = InGameTimer.getInstance();
-        if (!this.isClient() && m == 2 && block == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && dimension instanceof OverworldDimension) {
+        if (!this.isClient && m == 2 && block == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && dimension instanceof OverworldDimension) {
             for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
                 if (runPortalPos.squaredDistanceTo(new Vec3i(i, j, k)) < 100) {
                     return result;
                 }
             }
+            SpeedRunIGT.debug("Detected serverworld end portal");
             timer.getEndPortalPosList().add(new RunPortalPos(new Vec3i(i, j, k)));
             InGameTimerUtils.IS_KILLED_ENDER_DRAGON = false;
         }
-        return result;
-    }
 
-    public boolean isClient() {
-        return this.isClient;
+        return result;
     }
 }
