bc3afcde9740158e942fb290dca8a757791a46e0
RedLime <redlimerl@gmail.com>
2022-05-03 19:57:05 +0900

    fix all portals timing

--- src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
@@ -18,12 +18,11 @@ import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 
 import java.util.function.BiFunction;
-import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World {
 
-    protected ServerWorldMixin(LevelProperties levelProperties, DimensionType dimensionType, BiFunction<World, Dimension, ChunkManager> chunkManagerProvider, Supplier<Profiler> profiler, boolean isClient) {
+    protected ServerWorldMixin(LevelProperties levelProperties, DimensionType dimensionType, BiFunction<World, Dimension, ChunkManager> chunkManagerProvider, Profiler profiler, boolean isClient) {
         super(levelProperties, dimensionType, chunkManagerProvider, profiler, isClient);
     }
 
