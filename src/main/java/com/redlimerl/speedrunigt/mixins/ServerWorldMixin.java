65b1b2ffc49e666fb8f20003e8c05bb35ce901ae
RedLime <redlimerl@gmail.com>
2022-05-03 19:34:48 +0900

    fix all portals timing

--- src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ServerWorldMixin.java
@@ -1,39 +1,48 @@
 package com.redlimerl.speedrunigt.mixins;
 
+import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
 import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.ChunkManager;
 import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(World.class)
-public abstract class WorldMixin {
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
-    @Shadow public abstract boolean isClient();
+@Mixin(ServerWorld.class)
+public abstract class ServerWorldMixin extends World {
 
-    @Shadow public abstract Dimension getDimension();
+    protected ServerWorldMixin(LevelProperties levelProperties, DimensionType dimensionType, BiFunction<World, Dimension, ChunkManager> chunkManagerProvider, Supplier<Profiler> profiler, boolean isClient) {
+        super(levelProperties, dimensionType, chunkManagerProvider, profiler, isClient);
+    }
 
-    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", at = @At("TAIL"))
-    public void onUpdateBlockState(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> cir) {
+    @Override
+    public boolean setBlockState(BlockPos pos, BlockState state, int flags) {
+        boolean result = super.setBlockState(pos, state, flags);
+
         InGameTimer timer = InGameTimer.getInstance();
         if (!this.isClient() && flags == 2 && state.getBlock() == Blocks.END_PORTAL && timer.getCategory() == RunCategories.ALL_PORTALS && timer.getCategory() == RunCategories.ALL_PORTALS && getDimension().getType() == DimensionType.OVERWORLD) {
             for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
                 if (runPortalPos.squaredDistanceTo(pos) < 100) {
-                    return;
+                    return result;
                 }
             }
+            SpeedRunIGT.debug("Detected serverworld end portal");
             timer.getEndPortalPosList().add(new RunPortalPos(pos));
             InGameTimerUtils.IS_KILLED_ENDER_DRAGON = false;
         }
+
+        return result;
     }
 }
