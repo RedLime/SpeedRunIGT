189855c2c37897ce7c2889ded5e690d5d4029efd
RedLime <redlimerl@gmail.com>
2022-06-16 16:40:19 +0900

    10.10

---
+++ src/main/java/com/redlimerl/speedrunigt/mixins/screen/GeneratorOptionsMixin.java
@@ -0,0 +1,20 @@
+package com.redlimerl.speedrunigt.mixins.screen;
+
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import net.minecraft.world.gen.GeneratorOptions;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.OptionalLong;
+
+@Mixin(GeneratorOptions.class)
+public class GeneratorOptionsMixin {
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    @Inject(method = "withHardcore", at = @At("HEAD"))
+    public void onGenerate(boolean hardcore, OptionalLong optionalLong, CallbackInfoReturnable<GeneratorOptions> cir) {
+        InGameTimerUtils.IS_SET_SEED = optionalLong.isPresent();
+    }
+}
