5ce50108d016aa366e8012273e278caad76e069c
RedLime <redlimerl@gmail.com>
2022-06-20 17:45:06 +0900

    10.11 fix

--- src/main/java/com/redlimerl/speedrunigt/impl/OptionButtonsImpl.java
+++ src/main/java/com/redlimerl/speedrunigt/impl/OptionButtonsImpl.java
@@ -281,11 +281,11 @@ public class OptionButtonsImpl implements SpeedRunIGTApi {
 
         factories.add(screen -> new OptionButtonFactory.Builder()
                 .setButtonWidget(
-                        new ButtonWidget(0, 0, 150, 20, new TranslatableText("speedrunigt.option.debug_mode").append(" : ").append(SpeedRunIGT.IS_DEBUG_MODE ? ScreenTexts.ON : ScreenTexts.OFF),
+                        new ButtonWidget(0, 0, 150, 20, new TranslatableText("speedrunigt.option.debug_mode").append(" : ").append(SpeedRunIGT.IS_DEBUG_MODE ? ScreenTexts.ON : ScreenTexts.OFF).asFormattedString(),
                                 (ButtonWidget button) -> {
                                     SpeedRunIGT.IS_DEBUG_MODE = !SpeedRunIGT.IS_DEBUG_MODE;
                                     SpeedRunIGT.error("Debug mode is " + (SpeedRunIGT.IS_DEBUG_MODE ? "enabled" : "disabled") + "!");
-                                    button.setMessage(new TranslatableText("speedrunigt.option.debug_mode").append(" : ").append(SpeedRunIGT.IS_DEBUG_MODE ? ScreenTexts.ON : ScreenTexts.OFF));
+                                    button.setMessage(new TranslatableText("speedrunigt.option.debug_mode").append(" : ").append(SpeedRunIGT.IS_DEBUG_MODE ? ScreenTexts.ON : ScreenTexts.OFF).asFormattedString());
                                 })
                 )
                 .setToolTip(() -> I18n.translate("speedrunigt.option.debug_mode.description"))
