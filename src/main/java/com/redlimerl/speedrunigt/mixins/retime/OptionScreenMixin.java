efba4e50dab795b8f14f183d7e8852316166dadb
RedLime <redlimerl@gmail.com>
2022-07-11 16:47:40 +0900

    fix difficulty detection

--- src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionScreenMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionScreenMixin.java
@@ -1,5 +1,6 @@
 package com.redlimerl.speedrunigt.mixins.retime;
 
+import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import net.minecraft.client.gui.screen.options.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
@@ -11,8 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(OptionsScreen.class)
 public class OptionScreenMixin {
 
-    @Inject(method = "method_19833", at = @At("TAIL"))
+    @Inject(method = "method_19833", remap = false, at = @At("TAIL"))
     public void onChangeDifficulty(ButtonWidget buttonWidget, CallbackInfo ci) {
+        SpeedRunIGT.debug("detected");
         InGameTimerUtils.CHANGED_OPTIONS.add(buttonWidget);
     }
 
