f83654fe93c07754261eb085c51de1d1e74beef4
RedLime <redlimerl@gmail.com>
2022-07-11 16:56:43 +0900

    10.14

---
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionScreenMixin.java
@@ -0,0 +1,21 @@
+package com.redlimerl.speedrunigt.mixins.retime;
+
+import com.redlimerl.speedrunigt.SpeedRunIGT;
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OptionsScreen.class)
+public class OptionScreenMixin {
+
+    @Inject(method = "method_19833", remap = false, at = @At("TAIL"))
+    public void onChangeDifficulty(ButtonWidget buttonWidget, CallbackInfo ci) {
+        SpeedRunIGT.debug("detected");
+        InGameTimerUtils.CHANGED_OPTIONS.add(buttonWidget);
+    }
+
+}
