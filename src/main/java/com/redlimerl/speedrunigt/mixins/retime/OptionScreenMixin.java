0903708d4aab5ba8cf37be72b349520c7ac40f76
RedLime <redlimerl@gmail.com>
2022-07-11 17:01:50 +0900

    fix diff

--- src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionScreenMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionScreenMixin.java
@@ -4,6 +4,8 @@ import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.world.Difficulty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(OptionsScreen.class)
 public class OptionScreenMixin {
 
-    @Inject(method = "method_19833", remap = false, at = @At("TAIL"))
-    public void onChangeDifficulty(ButtonWidget buttonWidget, CallbackInfo ci) {
+    @Inject(method = "method_32632", remap = false, at = @At("TAIL"))
+    public void onChangeDifficulty(CyclingButtonWidget<?> button, Difficulty difficulty, CallbackInfo ci) {
         SpeedRunIGT.debug("detected");
-        InGameTimerUtils.CHANGED_OPTIONS.add(buttonWidget);
+        InGameTimerUtils.CHANGED_OPTIONS.add(button);
     }
 
 }
