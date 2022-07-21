a3342ee6742535e051805c8f8dc2c01e232e7491
RedLime <redlimerl@gmail.com>
2022-07-21 13:03:36 +0900

    10.16

--- src/main/java/com/redlimerl/speedrunigt/mixins/EndPortalBlockMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/EndPortalBlockMixin.java
@@ -5,6 +5,7 @@ import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.TimerStatus;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
+import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.EndPortalBlock;
 import net.minecraft.entity.Entity;
@@ -20,12 +21,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EndPortalBlock.class)
 public class EndPortalBlockMixin {
 
-    @Inject(method = "method_416", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;method_15562(Lnet/minecraft/class_3793;)Lnet/minecraft/entity/Entity;", shift = At.Shift.AFTER))
+    @Inject(method = "method_416", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;method_15562(Lnet/minecraft/class_3793;)Lnet/minecraft/entity/Entity;", shift = At.Shift.BEFORE))
     public void onCollisionPlayer(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
         if (entity instanceof PlayerEntity && world instanceof ServerWorld) {
             InGameTimer timer = InGameTimer.getInstance();
 
             //All Portals
+            if (entity.world.dimension.isOverworld()) {
+                boolean isNewPortal = true;
+                for (RunPortalPos runPortalPos : timer.getEndPortalPosList()) {
+                    if (runPortalPos.squaredDistanceTo(pos) < 100) {
+                        isNewPortal = false;
+                        break;
+                    }
+                }
+                if (isNewPortal) {
+                    timer.getEndPortalPosList().add(new RunPortalPos(pos));
+                    timer.tryInsertNewTimeline("portal_no_"+timer.getEndPortalPosList().size());
+                    InGameTimerUtils.IS_KILLED_ENDER_DRAGON = false;
+                }
+            }
+
             SpeedRunIGT.debug("Current portals : " + timer.getEndPortalPosList().size());
             if (InGameTimerUtils.IS_KILLED_ENDER_DRAGON && timer.getStatus() != TimerStatus.NONE
                     && timer.getCategory() == RunCategories.ALL_PORTALS && timer.getEndPortalPosList().size() >= 128) {
