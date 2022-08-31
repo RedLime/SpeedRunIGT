fabd55408e5a8d06a374e79bcdfedb1518bf3ba2
RedLime <redlimerl@gmail.com>
2022-08-31 14:50:15 +0900

    11.2

--- src/main/java/com/redlimerl/speedrunigt/mixins/timeline/SoundSystemMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/timeline/SoundSystemMixin.java
@@ -3,7 +3,7 @@ package com.redlimerl.speedrunigt.mixins.timeline;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundSystem;
-import net.minecraft.sound.Sounds;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +14,7 @@ public class SoundSystemMixin {
 
     @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("RETURN"))
     public void onSoundPlay(SoundInstance soundInstance, CallbackInfo ci) {
-        if (soundInstance.getIdentifier().equals(Sounds.ENTITY_VILLAGER_AMBIENT.getId()) && InGameTimer.getInstance().isPlaying()) {
+        if (soundInstance.getIdentifier().equals(new Identifier("mob.villager.idle")) && InGameTimer.getInstance().isPlaying()) {
             InGameTimer.getInstance().tryInsertNewTimeline("found_villager");
         }
     }
