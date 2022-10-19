57758d5448f486dcfa10f77df7433d77b7493796
RedLime <redlimerl@gmail.com>
2022-10-19 16:49:17 +0900

    12.2

--- src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
+++ src/main/java/com/redlimerl/speedrunigt/timer/InGameTimerUtils.java
@@ -15,7 +15,7 @@ import com.redlimerl.speedrunigt.timer.logs.TimerPauseLog;
 import com.redlimerl.speedrunigt.timer.logs.TimerTimeline;
 import com.redlimerl.speedrunigt.timer.running.RunPortalPos;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.impl.FabricLoaderImpl;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -200,7 +200,12 @@ public class InGameTimerUtils {
     }
 
     public static String getMinecraftVersion() {
-        return FabricLoaderImpl.INSTANCE.getGameProvider().getNormalizedGameVersion();
+        Optional<ModContainer> mcContainer = FabricLoader.getInstance().getModContainer("minecraft");
+        if (mcContainer.isPresent()) {
+            ModContainer mc = mcContainer.get();
+            return mc.getMetadata().getVersion().getFriendlyString();
+        }
+        return "unknown";
     }
 
     public static boolean isLoadableBlind(Dimension dimensionType, Vec3d netherPos, Vec3d overPos) {
