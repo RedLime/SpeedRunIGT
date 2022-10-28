ef930fe1ab5a85081d8de1a3cfb130908d6fdffb
RedLime <redlimerl@gmail.com>
2022-10-28 12:15:30 +0900

    fix rejoin pause issue

--- src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
@@ -111,10 +111,7 @@ public abstract class MinecraftClientMixin {
         if (timer.getStatus() == TimerStatus.NONE) return;
 
         InGameTimerUtils.IS_CHANGING_DIMENSION = false;
-
-        if (timer.getStatus() != TimerStatus.NONE) {
-            timer.setPause(true, TimerStatus.IDLE, "changed dimension");
-        }
+        timer.setPause(true, TimerStatus.IDLE, "changed dimension");
 
         // For Timelines
         if (targetWorld.getDimensionRegistryKey() == DimensionType.THE_NETHER_REGISTRY_KEY) {
