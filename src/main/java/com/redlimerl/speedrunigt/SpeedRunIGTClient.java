bff4fcbd480b40a88876091e90d6ba4ed1026556
RedLime <redlimerl@gmail.com>
2022-11-02 18:46:52 +0900

    13.0

--- src/main/java/com/redlimerl/speedrunigt/SpeedRunIGTClient.java
+++ src/main/java/com/redlimerl/speedrunigt/SpeedRunIGTClient.java
@@ -4,10 +4,10 @@ import com.redlimerl.speedrunigt.api.OptionButtonFactory;
 import com.redlimerl.speedrunigt.api.SpeedRunIGTApi;
 import com.redlimerl.speedrunigt.impl.OptionButtonsImpl;
 import com.redlimerl.speedrunigt.option.SpeedRunOption;
+import com.redlimerl.speedrunigt.therun.TheRunKeyHelper;
 import com.redlimerl.speedrunigt.timer.TimerDrawer;
 import com.redlimerl.speedrunigt.utils.FontUtils;
 import com.redlimerl.speedrunigt.utils.KeyBindingRegistry;
-import com.redlimerl.speedrunigt.utils.TranslateHelper;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
@@ -47,13 +47,6 @@ public class SpeedRunIGTClient implements ClientModInitializer {
         // End initializing
         isInitialized = true;
 
-        // Translate initialize
-        try {
-            TranslateHelper.init();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
         // Key Bindings initialize
         timerResetKeyBinding = KeyBindingRegistry.registerKeyBinding(new KeyBinding(
                 "speedrunigt.controls.start_timer",
@@ -70,6 +63,8 @@ public class SpeedRunIGTClient implements ClientModInitializer {
 
         // Add default fonts
         FontUtils.copyDefaultFonts();
+
+        TheRunKeyHelper.load();
 
         SpeedRunIGT.IS_CLIENT_SIDE = true;
     }
--- src/main/java/com/redlimerl/speedrunigt/gui/screen/SpeedRunOptionScreen.java
+++ src/main/java/com/redlimerl/speedrunigt/gui/screen/SpeedRunOptionScreen.java
@@ -70,7 +70,7 @@ public class SpeedRunOptionScreen extends Screen {
 
         nextPageButton = addButton(new ButtonWidget(width - 68, 30 + (6 * 22), 38, 20, ">", (ButtonWidget button) -> openPage(+1)));
 
-        openPage(0);
+        openPage(page);
 
         addButton(new ButtonWidget(width - 85, height - 35, 70, 20, ScreenTexts.CANCEL, (ButtonWidget button) -> onClose()));
 
@@ -81,7 +81,8 @@ public class SpeedRunOptionScreen extends Screen {
         buttonListWidget = new ButtonScrollListWidget();
         children.add(buttonListWidget);
 
-        categorySelectButtons.keySet().stream().findFirst().ifPresent(this::selectCategory);
+        if (!currentSelectCategory.isEmpty()) selectCategory(currentSelectCategory);
+        else categorySelectButtons.keySet().stream().findFirst().ifPresent(this::selectCategory);
     }
 
     public void openPage(int num) {
