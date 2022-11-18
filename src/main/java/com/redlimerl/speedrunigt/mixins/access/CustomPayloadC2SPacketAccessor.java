b088b6b272649bf3dd958510faebe19bfeffb1f2
wurgo <laxvideoeffects@gmail.com>
2022-11-18 17:16:15 +0000

    Move accessor

--- src/main/java/com/redlimerl/speedrunigt/mixins/access/CustomPayloadC2SPacketAccessor.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/access/CustomPayloadC2SPacketAccessor.java
@@ -1,4 +1,4 @@
-package com.redlimerl.speedrunigt.mixins.network;
+package com.redlimerl.speedrunigt.mixins.access;
 
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
@@ -8,10 +8,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(CustomPayloadC2SPacket.class)
 public interface CustomPayloadC2SPacketAccessor {
-
-    @Accessor("channel")
-    Identifier getChannel();
-
-    @Accessor("data")
-    PacketByteBuf getData();
+    @Accessor("channel") Identifier getChannel();
+    @Accessor("data") PacketByteBuf getData();
 }
