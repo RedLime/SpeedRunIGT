fe800c8778df08ed183214dd4fe9515043dfbf6f
RedLime <redlimerl@gmail.com>
2022-12-07 13:39:06 +0900

    fix retime

--- src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionButtonWidgetMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionButtonWidgetMixin.java
@@ -1,27 +1,22 @@
 package com.redlimerl.speedrunigt.mixins.retime;
 
-import com.redlimerl.speedrunigt.mixins.retime.accessor.OptionSliderWidgetAccessor;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.SettingsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.OptionButtonWidget;
-import net.minecraft.client.gui.widget.OptionSliderWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Screen.class)
+@Mixin(value = { Screen.class, SettingsScreen.class })
 public class OptionButtonWidgetMixin {
 
-    @SuppressWarnings("ConstantConditions")
     @Inject(method = "method_21930", at = @At("RETURN"))
     public void onClickOption(ButtonWidget button, CallbackInfo ci) {
-        if (((Object) this) instanceof OptionButtonWidget) {
-            InGameTimerUtils.CHANGED_OPTIONS.add(((OptionButtonWidget) ((Object) this)).method_1088());
-        }
-        if (((Object) this) instanceof OptionSliderWidget) {
-            InGameTimerUtils.CHANGED_OPTIONS.add(((OptionSliderWidgetAccessor) this).getOption());
+        if (button instanceof OptionButtonWidget) {
+            InGameTimerUtils.CHANGED_OPTIONS.add(((OptionButtonWidget) button).method_1088());
         }
     }
 }
