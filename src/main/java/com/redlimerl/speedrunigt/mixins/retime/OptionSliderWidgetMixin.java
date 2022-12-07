a8d55e96d9a2b87a3d7185a5336794006d887644
RedLime <redlimerl@gmail.com>
2022-12-07 13:39:06 +0900

    fix retime

---
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionSliderWidgetMixin.java
@@ -0,0 +1,22 @@
+package com.redlimerl.speedrunigt.mixins.retime;
+
+import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
+import net.minecraft.client.gui.widget.OptionSliderWidget;
+import net.minecraft.client.options.GameOption;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(OptionSliderWidget.class)
+public class OptionSliderWidgetMixin {
+
+
+    @Shadow private GameOption field_7740;
+
+    @Inject(method = "method_21886", at = @At("RETURN"))
+    public void onClickOption(int mouseY, int par2, CallbackInfo ci) {
+        InGameTimerUtils.CHANGED_OPTIONS.add(this.field_7740);
+    }
+}
