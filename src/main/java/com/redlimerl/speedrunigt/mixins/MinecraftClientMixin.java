c3a9defcdcf1ff95a9d9b630567c0d2b87953f31
RedLime <redlimerl@gmail.com>
2022-12-21 14:33:09 +0900

    fix render issue

--- src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/MinecraftClientMixin.java
@@ -122,7 +122,7 @@ public abstract class MinecraftClientMixin {
         }
     }
 
-    @Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/MetricsData;pushSample(J)V", shift = At.Shift.BEFORE))
+    @Inject(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_3264;method_14490(Lnet/minecraft/client/util/Window;)V", shift = At.Shift.AFTER))
     private void renderMixin(CallbackInfo ci) {
         InGameTimer timer = InGameTimer.getInstance();
 
