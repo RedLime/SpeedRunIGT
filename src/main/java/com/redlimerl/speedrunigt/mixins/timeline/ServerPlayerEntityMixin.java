c363cc0f8a659e8e0b9a1d5ceaa1669cea2149ec
RedLime <redlimerl@gmail.com>
2023-04-17 03:11:29 +0900

    13.4.1

--- src/main/java/com/redlimerl/speedrunigt/mixins/timeline/ServerPlayerEntityMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/timeline/ServerPlayerEntityMixin.java
@@ -1,12 +1,13 @@
 package com.redlimerl.speedrunigt.mixins.timeline;
 
-import com.redlimerl.speedrunigt.SpeedRunIGT;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.InGameTimerUtils;
 import com.redlimerl.speedrunigt.timer.TimerStatus;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.PlayerManager;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.dimension.DimensionType;
@@ -40,11 +41,30 @@ public abstract class ServerPlayerEntityMixin {
             }
 
             if (oldDimension == DimensionType.NETHER && newDimension == DimensionType.OVERWORLD) {
-                if (InGameTimerUtils.isBlindTraveled(lastPortalPos)) {
+                if (this.isEnoughTravel(player)) {
+                    int portalIndex = InGameTimerUtils.isBlindTraveled(lastPortalPos);
                     InGameTimer.getInstance().tryInsertNewTimeline("nether_travel");
+                    if (portalIndex == 0) {
+                        InGameTimer.getInstance().tryInsertNewTimeline("nether_travel_home");
+                    } else {
+                        InGameTimer.getInstance().tryInsertNewTimeline("nether_travel_blind");
+                    }
                 }
                 if (!timer.isCoop() && InGameTimer.getInstance().getCategory() == RunCategories.ANY) InGameTimerUtils.IS_CAN_WAIT_WORLD_LOAD = InGameTimerUtils.isLoadableBlind(DimensionType.OVERWORLD, lastPortalPos.add(0, 0, 0), player.getPos().add(0, 0, 0));
             }
         }
+    }
+
+    private boolean isEnoughTravel(ServerPlayerEntity serverPlayerEntity) {
+        boolean eye = false, pearl = false, rod = false;
+        for (ItemStack itemStack : serverPlayerEntity.inventory.main) {
+            if (itemStack != null) {
+                if (itemStack.getItem() == Items.EYE_OF_ENDER) eye = true;
+                if (itemStack.getItem() == Items.ENDER_PEARL) pearl = true;
+                if (itemStack.getItem() == Items.BLAZE_POWDER || itemStack.getItem() == Items.BLAZE_ROD) rod = true;
+            }
+        }
+
+        return eye || (pearl && rod);
     }
 }
