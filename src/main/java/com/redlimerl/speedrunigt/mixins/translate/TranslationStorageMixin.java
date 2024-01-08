56b2c871148d7ab639897cf8ced143e6d48ec90c
tildejustin <spacepepper3.14@gmail.com>
2024-01-08 15:37:27 -0500

    optimize translation loading impl (fixes quilt incompatibility)

--- src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
@@ -2,9 +2,10 @@ package com.redlimerl.speedrunigt.mixins.translate;
 
 import com.redlimerl.speedrunigt.option.SpeedRunOption;
 import com.redlimerl.speedrunigt.option.SpeedRunOptions;
-import com.redlimerl.speedrunigt.utils.TranslateHelper;
+import com.redlimerl.speedrunigt.utils.ResourcesHelper;
 import net.minecraft.client.resource.language.TranslationStorage;
 import net.minecraft.resource.Resource;
+import net.minecraft.util.Language;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,12 +13,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Mixin(TranslationStorage.class)
 public class TranslationStorageMixin {
 
     @Inject(method = "load(Ljava/util/List;Ljava/util/Map;)V", at = @At("RETURN"))
     private static void onLoad(List<Resource> resources, Map<String, String> translationMap, CallbackInfo ci) {
-        TranslateHelper.setup(resources, translationMap::put, SpeedRunOption.getOption(SpeedRunOptions.ALWAYS_ENGLISH_TRANSLATIONS));
+        resources.forEach(resource -> {
+            // minecraft always loads en_us as a backup, if using only english translations just skip loading the other attempts
+            if (SpeedRunOption.getOption(SpeedRunOptions.ALWAYS_ENGLISH_TRANSLATIONS) && !resource.getId().getPath().equalsIgnoreCase("lang/en_us.json"))
+                return;
+            Optional.ofNullable(ResourcesHelper.toStream("/assets/speedrunigt/" + resource.getId().getPath()))
+                    .ifPresent(langStream -> Language.load(langStream, translationMap::put));
+        });
     }
 }
--- src/main/java/com/redlimerl/speedrunigt/utils/TranslateHelper.java
+++ src/main/java/com/redlimerl/speedrunigt/utils/TranslateHelper.java
@@ -1,39 +1,8 @@
 package com.redlimerl.speedrunigt.utils;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.resource.Resource;
-import net.minecraft.util.Language;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.function.BiConsumer;
 
 public class TranslateHelper {
-    public static String[] getLangFileNames() throws IOException, URISyntaxException {
-        return ResourcesHelper.getResourceChildren("lang");
-    }
-
-    public static void setup(List<Resource> resources, BiConsumer<String, String> biConsumer, boolean englishOnly) {
-        try {
-            for (String langFileName : getLangFileNames()) {
-                if (englishOnly && !langFileName.endsWith("en_us.json")) continue;
-
-                for (Resource resource : resources) {
-                    if (!langFileName.endsWith(resource.getId().getPath())) continue;
-
-                    InputStream inputStream = TranslateHelper.class.getResourceAsStream(langFileName);
-                    if (inputStream != null) {
-                        Language.load(inputStream, biConsumer);
-                    }
-                }
-            }
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void reload() {
         MinecraftClient.getInstance().reloadResources();
     }
