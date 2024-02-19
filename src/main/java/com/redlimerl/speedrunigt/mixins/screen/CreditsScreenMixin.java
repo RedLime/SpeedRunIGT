8c5e96c2f88afdf60d0154005a6fdf25e268027b
tildejustin <spacepepper3.14@gmail.com>
2024-02-19 00:40:59 -0500

    fix in-game credits button stopping timer

--- src/main/java/com/redlimerl/speedrunigt/mixins/screen/CreditsScreenMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/screen/CreditsScreenMixin.java
@@ -7,7 +7,9 @@ import com.redlimerl.speedrunigt.timer.TimerStatus;
 import com.redlimerl.speedrunigt.timer.category.RunCategories;
 import net.minecraft.client.gui.screen.CreditsScreen;
 import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,14 +17,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(CreditsScreen.class)
 public class CreditsScreenMixin {
 
+    @Shadow
+    @Final
+    private boolean endCredits;
+
     @Inject(method = "init()V", at = @At("TAIL"))
     private void initMixin(CallbackInfo ci) {
+        if (!this.endCredits) {
+            return;
+        }
+
         @NotNull
         InGameTimer timer = InGameTimer.getInstance();
         if (timer.getStatus() != TimerStatus.NONE) {
             int anyToAATime = SpeedRunOption.getOption(SpeedRunOptions.CHANGE_ANY_TO_AA_OVER);
             if (anyToAATime > 0 && (timer.getCategory() == RunCategories.ANY || timer.getCategory() == RunCategories.ALL_ADVANCEMENTS)) {
-                if (timer.getInGameTime() < 1000L*60*anyToAATime) {
+                if (timer.getInGameTime() < 1000L * 60 * anyToAATime) {
                     timer.setCategory(RunCategories.ANY, true);
                 } else {
                     timer.setCategory(RunCategories.ALL_ADVANCEMENTS, true);
