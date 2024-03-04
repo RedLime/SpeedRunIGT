b73a5e06041a18e073eee073d5725f992e05a1cf
tildejustin <spacepepper3.14@gmail.com>
2024-03-04 10:43:33 -0500

    migrate mappings to 1.9.4+build.541

--- src/main/java/com/redlimerl/speedrunigt/mixins/keybinding/KeyBindingAccessor.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/keybinding/KeyBindingAccessor.java
@@ -1,10 +1,10 @@
 package com.redlimerl.speedrunigt.mixins.keybinding;
 
-import net.minecraft.client.options.KeyBinding;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Set;
+import net.minecraft.client.option.KeyBinding;
 
 @Mixin(KeyBinding.class)
 public interface KeyBindingAccessor {
