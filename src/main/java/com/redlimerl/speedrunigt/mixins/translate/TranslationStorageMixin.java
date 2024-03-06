3e1b34260e527c6432ca04ab8292529876f698a0
tildejustin <spacepepper3.14@gmail.com>
2024-03-06 15:06:09 -0500

    translation fix

--- src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/translate/TranslationStorageMixin.java
@@ -10,9 +10,7 @@ import org.apache.commons.io.IOUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,14 +21,18 @@ import java.util.Properties;
 @Mixin(Language.class)
 public abstract class TranslationStorageMixin {
 
-    @Inject(method = "setCode", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Language;method_633(Ljava/util/Properties;Ljava/lang/String;)V", shift = At.Shift.BEFORE, ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void onLoad(String language, CallbackInfo ci, Properties properties) {
+    @Shadow
+    protected abstract void method_633(Properties properties, String string);
+
+    @Redirect(method = "setCode", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Language;method_633(Ljava/util/Properties;Ljava/lang/String;)V"))
+    private void onLoad(Language instance, Properties properties, String language) {
+        method_633(properties, language);
         InputStream inputStream = TranslateHelper.setup(language, SpeedRunOption.getOption(SpeedRunOptions.ALWAYS_ENGLISH_TRANSLATIONS));
         try {
             if (inputStream == null) return;
             String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
 
-                JsonElement jsonElement = new JsonParser().parse(json);
+            JsonElement jsonElement = new JsonParser().parse(json);
 
             for (Map.Entry<String, JsonElement> stringJsonElementEntry : jsonElement.getAsJsonObject().entrySet()) {
                 properties.setProperty(stringJsonElementEntry.getKey(), stringJsonElementEntry.getValue().getAsString());
@@ -39,5 +41,10 @@ public abstract class TranslationStorageMixin {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Redirect(method = "setCode", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z", ordinal = 0))
+    private boolean alwaysRefreshLanguageKeys(String instance, Object object) {
+        return false;
     }
 }
