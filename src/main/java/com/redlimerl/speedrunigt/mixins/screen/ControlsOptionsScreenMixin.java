3c11eabee5b2a9fcd5d7cd48ed1854bb6faff875
tildejustin <spacepepper3.14@gmail.com>
2024-03-06 16:18:20 -0500

    dynamic done buttton for ControlsOptionsScreen

---
+++ src/main/java/com/redlimerl/speedrunigt/mixins/screen/ControlsOptionsScreenMixin.java
@@ -0,0 +1,16 @@
+package com.redlimerl.speedrunigt.mixins.screen;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(ControlsOptionsScreen.class)
+public abstract class ControlsOptionsScreenMixin extends Screen {
+    @ModifyArg(method = "init", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIILjava/lang/String;)V"))
+    private int dynamicDoneButton(int num) {
+        return this.height / 6 + 24 * (((Minecraft.getMinecraft().options.allKeys.length + 2 - 1) / 2));
+    }
+}
