93a3e46953b684680f0f3ce906a27b5df9f80878
RedLime <redlimerl@gmail.com>
2024-04-24 01:31:50 +0900

    1.20.5

--- src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionSliderWidgetMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/retime/OptionSliderWidgetMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net.minecraft.client.option.SimpleOption$OptionSliderWidgetImpl")
+@Mixin(SimpleOption.OptionSliderWidgetImpl.class)
 public class OptionSliderWidgetMixin {
 
     @Shadow @Final
