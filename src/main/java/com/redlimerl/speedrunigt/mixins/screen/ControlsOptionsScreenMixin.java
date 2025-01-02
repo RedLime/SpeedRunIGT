f0f9eadeb97be615d8c17dae0ac7394c756f63f3
tildejustin <spacepepper3.14@gmail.com>
2025-01-02 09:50:38 -0500

    support legacy fabric api (#214)

--- src/main/java/com/redlimerl/speedrunigt/mixins/screen/ControlsOptionsScreenMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/screen/ControlsOptionsScreenMixin.java
@@ -1,6 +1,6 @@
 package com.redlimerl.speedrunigt.mixins.screen;
 
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class ControlsOptionsScreenMixin extends Screen {
     @ModifyArg(method = "init", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIILjava/lang/String;)V"))
     private int dynamicDoneButton(int num) {
-        return this.height / 6 + 24 * (((MinecraftClient.getInstance().options.allKeys.length + 2 - 1) / 2));
+        return !FabricLoader.getInstance().isModLoaded("legacy-fabric-api") ? this.height / 6 + 24 * ((this.client.options.allKeys.length + 2 - 1) / 2) : num;
     }
 }
