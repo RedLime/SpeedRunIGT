b74fb368087da4fb507fedf3f1ee0d1f42061231
tildejustin <spacepepper3.14@gmail.com>
2025-06-22 12:38:58 -0400

    revert changes to stronghold detection

--- src/main/java/com/redlimerl/speedrunigt/mixins/timeline/ServerPlayerEntityMixin2.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/timeline/ServerPlayerEntityMixin2.java
@@ -1,7 +1,6 @@
 package com.redlimerl.speedrunigt.mixins.timeline;
 
 import com.redlimerl.speedrunigt.mixins.access.ServerChunkProviderAccessor;
-import com.redlimerl.speedrunigt.mixins.access.StructureFeatureAccessor;
 import com.redlimerl.speedrunigt.mixins.access.SurfaceChunkGeneratorAccessor;
 import com.redlimerl.speedrunigt.timer.InGameTimer;
 import com.redlimerl.speedrunigt.timer.TimerStatus;
@@ -11,12 +10,10 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.NetherFortressStructure;
 import net.minecraft.structure.StrongholdStructure;
-import net.minecraft.structure.class_21;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.NetherChunkGenerator;
 import net.minecraft.world.dimension.OverworldDimension;
 import net.minecraft.world.dimension.TheNetherDimension;
-import net.minecraft.world.gen.GeneratorConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -46,20 +43,7 @@ public abstract class ServerPlayerEntityMixin2 {
             if (this.getServerWorld().dimension instanceof OverworldDimension) {
                 StrongholdStructure strongholdStructure = ((SurfaceChunkGeneratorAccessor) ((ServerChunkProviderAccessor) this.getServerWorld().getChunkProvider()).getChunkGenerator()).getStrongholdGenerator();
                 boolean isInStronghold = strongholdStructure.method_46(this.method_4086().x, this.method_4086().y + 1, this.method_4086().z);
-                boolean inPortalRoom = false;
                 if (isInStronghold) {
-                    // method_46 already checks method_5516 != null so npe is not of concern
-                    GeneratorConfig config = ((StructureFeatureAccessor) strongholdStructure).invokeMethod_5516(this.method_4086().x, this.method_4086().y + 1, this.method_4086().z);
-                    for (Object piece : config.getChildren()) {
-                        // class_21 is the portal room
-                        if (piece instanceof class_21) {
-                            inPortalRoom = (((class_21) piece).getBoundingBox().intersects(this.method_4086().x, this.method_4086().y + 1, this.method_4086().z));
-                            break;
-                        }
-                    }
-                }
-                boolean onStoneBrick = standingOn == Blocks.STONE_BRICKS || standingOn == Blocks.MONSTER_EGG;
-                if (inPortalRoom || isInStronghold && onStoneBrick) {
                     timer.tryInsertNewTimeline("found_stronghold");
                 }
             }
