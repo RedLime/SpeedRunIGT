f742cbaf8156f04b85df6971a8962aff82cac8e9
tildejustin <spacepepper3.14@gmail.com>
2025-07-27 22:40:08 -0400

    use fabric loader api for resource loading (#230)

--- src/main/java/com/redlimerl/speedrunigt/utils/ResourcesHelper.java
+++ src/main/java/com/redlimerl/speedrunigt/utils/ResourcesHelper.java
@@ -1,36 +1,43 @@
 package com.redlimerl.speedrunigt.utils;
 
-import java.io.FileNotFoundException;
+import net.fabricmc.loader.api.FabricLoader;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class ResourcesHelper {
+    private static final Path root;
+
+    static {
+        @SuppressWarnings("OptionalGetWithoutIsPresent")
+        List<Path> paths = FabricLoader.getInstance().getModContainer("speedrunigt").get().getRootPaths();
+        if (paths.size() != 1) {
+            throw new IllegalStateException("unknown jar type");
+        }
+        root = paths.get(0);
+    }
+
     public static String[] getResourceChildren(String folder) throws IOException, URISyntaxException {
-        URL parent = ResourcesHelper.class.getResource("/" + folder);
-        if (parent == null) {
-            throw new FileNotFoundException(folder);
-        }
-        Path path;
-        if (parent.getProtocol().equals("jar")) {
-            // someone already opened the jar, probably fabric loader
-            FileSystem fs = FileSystems.getFileSystem(parent.toURI());
-            if (fs == null) {
-                throw new FileSystemNotFoundException(parent.toURI().toString());
-            }
-            path = fs.getPath("/" + folder);
-        } else {
-            path = Paths.get(parent.toURI());
-        }
-        try (Stream<Path> children = Files.list(path)) {
+
+        try (Stream<Path> children = Files.list(root.resolve(folder))) {
             return children.map(inner -> inner.subpath(inner.getNameCount() - 2, inner.getNameCount())).map(Path::toString).toArray(String[]::new);
         }
     }
 
     public static InputStream toStream(String resource) {
-        return ResourcesHelper.class.getResourceAsStream(resource);
+        Path path = root.resolve(resource);
+        if (!Files.exists(path)) {
+            return null;
+        }
+        try {
+            return Files.newInputStream(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
