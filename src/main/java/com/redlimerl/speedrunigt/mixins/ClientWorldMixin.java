1c40d8f4e865051861d0311d2c58fb61c0c09e09
tildejustin <spacepepper3.14@gmail.com>
2025-08-03 22:22:58 -0400

    refactor MaC logic, work with nether not caves b/c hasCeiling does not account for that

--- src/main/java/com/redlimerl/speedrunigt/mixins/ClientWorldMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/ClientWorldMixin.java
@@ -12,7 +12,7 @@ import net.minecraft.world.Heightmap;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.EmptyChunk;
+import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.chunk.FlatChunkGenerator;
 import org.spongepowered.asm.mixin.Final;
@@ -23,13 +23,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 @Mixin(ClientWorld.class)
 public abstract class ClientWorldMixin extends World {
-    @Shadow
-    public abstract ClientWorld.Properties getLevelProperties();
-
     @Shadow
     @Final
     private MinecraftClient client;
@@ -43,67 +41,87 @@ public abstract class ClientWorldMixin extends World {
         InGameTimer.getInstance().tick();
     }
 
+    @Unique
+    private final int[][] heightmapAccumulator = new int[16 * 3][16 * 3];
+
     @Inject(method = "updateListeners", at = @At("TAIL"))
     public void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, int flags, CallbackInfo ci) {
-        // TODO: doesn't support nether or overworld caves
-        if (this.getDimension().hasCeiling()) {
+        InGameTimer timer = InGameTimer.getInstance();
+        if (timer.getCategory() != RunCategories.MINE_A_CHUNK) {
             return;
         }
 
-        InGameTimer timer = InGameTimer.getInstance();
-        if (timer.getCategory() == RunCategories.MINE_A_CHUNK) {
-            int chunkX = pos.getX() >> 4;
-            int chunkZ = pos.getZ() >> 4;
+        if (this.getDimensionRegistryKey() == DimensionType.THE_END_REGISTRY_KEY) {
+            return;
+        }
 
-            for (int i = -1; i < 2; ++i) {
-                for (int j = -1; j < 2; ++j) {
-                    // if all the chunks aren't loaded (and chunks are given as EmptyChunks), it will break because the heightmap is reported as all 0's
-                    if (getChunk(chunkX + i, chunkZ + j) instanceof EmptyChunk) {
-                        return;
-                    }
+        int chunkX = pos.getX() >> 4;
+        int chunkZ = pos.getZ() >> 4;
+
+        for (int i = -1; i < 2; ++i) {
+            for (int j = -1; j < 2; ++j) {
+                // if all the chunks aren't loaded, it will break because the heightmap is reported as all 0's
+                if (getChunk(chunkX + i, chunkZ + j, ChunkStatus.FULL, false) == null) {
+                    return;
                 }
             }
+        }
 
-            // checks for 16 x 16 squares that have blocks only at or below bedrock in the square from the chunks -1, -1 to 1, 1 relative to the chunk the block update was in
-            // algorithm from https://stackoverflow.com/a/17790267
-            int[][] heightmapAccumulator = new int[16 * 3][16 * 3];
+        // checks for 16 x 16 squares that have blocks only at or below bedrock in the square from the chunks -1, -1 to 1, 1 relative to the chunk the block update was in
+        // algorithm from https://stackoverflow.com/a/17790267
+        for (int[] arr : heightmapAccumulator) {
+            Arrays.fill(arr, 0);
+        }
 
-            for (int x = 0; x < 16 * 3; ++x) {
-                for (int z = 0; z < 16 * 3; ++z) {
-                    // convert the 0 to 47 x and z counters in to -1 to 1 chunk offsets
-                    Chunk chunk = this.getChunk(chunkX + (x >> 4) - 1, chunkZ + (z >> 4) - 1);
-                    int height = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE).get(mod(x, 16), mod(z, 16));
-                    if (height <= getBedrockMaxHeight()) {
-                        if (x == 0 || z == 0) {
-                            // special case for first row and column, no previous work to check
-                            heightmapAccumulator[x][z] = 1;
-                            continue;
+        boolean hasCeiling = this.getDimension().hasCeiling();
+        int firstNonBedrockLayer = this.getFirstNonBedrockLayer();
+        int lastNonBedrockLayer = this.getDimensionHeight() - 6;
+
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        for (int x = 0; x < 16 * 3; ++x) {
+            for (int z = 0; z < 16 * 3; ++z) {
+                boolean columnClear = true;
+                Chunk chunk = this.getChunk(chunkX + (x >> 4) - 1, chunkZ + (z >> 4) - 1, ChunkStatus.FULL, false);
+                assert chunk != null; // already checked at the start
+                if (hasCeiling) {
+                    // check the column manually, use mutable for less churn
+                    // calculate world coordinates
+                    mutable.set((chunkX << 4) - 16 + x, 0, (chunkZ << 4) - 16 + z);
+                    for (int y = firstNonBedrockLayer; y <= lastNonBedrockLayer; y++) {
+                        mutable.setY(y);
+                        if (!chunk.getBlockState(mutable).isAir()) {
+                            columnClear = false;
+                            break;
                         }
-                        //  calculate the max value the next square is allowed to be using the bounds of the previous adjacent ones
-                        int currentSquareLevel = Math.min(Math.min(heightmapAccumulator[x - 1][z], heightmapAccumulator[x][z - 1]), heightmapAccumulator[x - 1][z - 1]) + 1;
-                        // if we hit 16 on the square level, we've found a large enough area
-                        if (currentSquareLevel == 16) {
-                            InGameTimer.complete();
-                            return;
-                        }
-                        // otherwise, just assign the value to it's place in the matrix
-                        heightmapAccumulator[x][z] = currentSquareLevel;
-                    } else {
-                        heightmapAccumulator[x][z] = 0;
                     }
+                } else {
+                    // calculate chunk coordinates
+                    columnClear = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE).get(x & 15, z & 15) <= firstNonBedrockLayer;
+                }
+                if (columnClear) {
+                    if (x == 0 || z == 0) {
+                        // special case for first row and column, no previous work to check
+                        heightmapAccumulator[x][z] = 1;
+                        continue;
+                    }
+                    //  calculate the max value the next square is allowed to be using the bounds of the previous adjacent ones
+                    int currentSquareLevel = Math.min(Math.min(heightmapAccumulator[x - 1][z], heightmapAccumulator[x][z - 1]), heightmapAccumulator[x - 1][z - 1]) + 1;
+                    // if we hit 16 on the square level, we've found a large enough area
+                    if (currentSquareLevel == 16) {
+                        InGameTimer.complete();
+                        return;
+                    }
+                    // otherwise, just assign the value to it's place in the matrix
+                    heightmapAccumulator[x][z] = currentSquareLevel;
+                } else {
+                    heightmapAccumulator[x][z] = 0;
                 }
             }
         }
     }
 
-    // handles negative numbers correctly, like python
     @Unique
-    private static int mod(int divisor, int dividend) {
-        return ((divisor % dividend) + dividend) % dividend;
-    }
-
-    @Unique
-    private int getBedrockMaxHeight() {
+    private int getFirstNonBedrockLayer() {
         if (this.client.isIntegratedServerRunning() && this.client.getServer().getWorld((this.getRegistryKey())).getChunkManager().getChunkGenerator() instanceof FlatChunkGenerator) {
             return 1;
         }
@@ -127,7 +145,7 @@ public abstract class ClientWorldMixin extends World {
         for (int x = 0; x < 16 * 3; ++x) {
             for (int z = 0; z < 16 * 3; ++z) {
                 Chunk chunk = this.getChunk(chunkX + (x >> 4) - 1, chunkZ + (z >> 4) - 1);
-                int height = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE).get(mod(x, 16), mod(z, 16));
+                int height = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE).get(x & 15, z & 15);
                 System.out.printf("%02d ", height);
             }
             System.out.println();
