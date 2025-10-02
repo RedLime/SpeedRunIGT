a34c35e42aad8244c3566be52602f35f7d32a221
RedLime <redlimerl@gmail.com>
2025-10-03 00:47:06 +0900

    1.21.10 port

--- src/main/java/com/redlimerl/speedrunigt/mixins/EndPortalBlockMixin.java
+++ src/main/java/com/redlimerl/speedrunigt/mixins/EndPortalBlockMixin.java
@@ -23,7 +23,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class EndPortalBlockMixin {
 
     @Inject(method = "onEntityCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;tryUsePortal(Lnet/minecraft/block/Portal;Lnet/minecraft/util/math/BlockPos;)V", shift = At.Shift.BEFORE))
-    public void onCollisionPlayer(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, CallbackInfo ci) {
+    public void onCollisionPlayer(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, boolean bl, CallbackInfo ci) {
         if (entity instanceof PlayerEntity && world instanceof ServerWorld) {
             InGameTimer timer = InGameTimer.getInstance();
 
